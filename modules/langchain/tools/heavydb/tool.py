5e3e450ce6b05e2acf5d11b6dbced69098a9af10
Brendan Turner <bfturner@foglet.io>
2023-03-27 10:53:55 -0500

    Add langchain tools for interacting with HeavyDB

---
+++ modules/langchain/tools/heavydb/tool.py
@@ -0,0 +1,108 @@
+from langchain.chains.llm import LLMChain
+from langchain.llms.openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.tools.base import BaseTool
+from pydantic import BaseModel, Extra, Field, validator
+
+from modules.langchain import HeavyDB
+from modules.langchain.tools.heavydb.prompts import QUERY_CHECKER
+
+
+class BaseHeavyDBTool(BaseModel):
+    """Base tool for interacting with a HeavyDB database."""
+
+    db: HeavyDB = Field(exclude=True)
+
+    # Override BaseTool.Config to appease mypy
+    # See https://github.com/pydantic/pydantic/issues/4173
+    class Config(BaseTool.Config):
+        """Configuration for this pydantic object."""
+
+        arbitrary_types_allowed = True
+        extra = Extra.forbid
+
+
+class QueryHeavyDBTool(BaseHeavyDBTool, BaseTool):
+    """Tool for querying a HeavyDB database."""
+
+    name = "query_sql_db"
+    description = """
+    Input to this tool is a detailed and correct SQL query, output is a result from the database.
+    If the query is not correct, an error message will be returned.
+    If an error is returned, rewrite the query, check the query, and try again.
+    """
+
+    def _run(self, query: str) -> str:
+        """Execute the query, return the results or an error message."""
+        return self.db.run_no_throw(query)
+
+    async def _arun(self, query: str) -> str:
+        raise NotImplementedError("QueryHeavyDBTool does not support async")
+
+
+class InfoHeavyDBTool(BaseHeavyDBTool, BaseTool):
+    """Tool for getting metadata about a HeavyDB database."""
+
+    name = "schema_sql_db"
+    description = """
+    Input to this tool is a comma-separated list of tables, output is the schema and sample rows for those tables.
+    Be sure that the tables actually exist by calling list_tables_sql_db first!
+
+    Example Input: "table1, table2, table3"
+    """
+
+    def _run(self, table_names: str) -> str:
+        """Get the schema for tables in a comma-separated list."""
+        return self.db.get_table_info_no_throw(table_names.split(", "))
+
+    async def _arun(self, table_name: str) -> str:
+        raise NotImplementedError("SchemaSqlDbTool does not support async")
+
+
+class ListHeavyDBTool(BaseHeavyDBTool, BaseTool):
+    """Tool for listing tables in a HeavyDB database."""
+
+    name = "list_tables_sql_db"
+    description = """
+    Input to this tool is nothing, output is a list of tables in the database.
+    """
+
+    def _run(self) -> str:
+        """Get the list of tables in the database."""
+        return self.db.get_table_info_no_throw()
+
+    async def _arun(self) -> str:
+        raise NotImplementedError("ListSqlDbTool does not support async")
+
+
+# This may need to be adapted to check HeavyDB's specific SQL dialect
+class QueryCheckerTool(BaseHeavyDBTool, BaseTool):
+    """Use an LLM to check if a query is correct.
+    Adapted from https://www.patterns.app/blog/2023/01/18/crunchbot-sql-analyst-gpt/"""
+
+    template: str = QUERY_CHECKER
+    llm_chain: LLMChain = Field(
+        default_factory=lambda: LLMChain(
+            llm=OpenAI(temperature=0),
+            prompt=PromptTemplate(template=QUERY_CHECKER, input_variables=["query", "dialect"]),
+        )
+    )
+    name = "query_checker_sql_db"
+    description = """
+    Use this tool to double check if your query is correct before executing it.
+    Always use this tool before executing a query with query_sql_db!
+    """
+
+    @validator("llm_chain")
+    def validate_llm_chain_input_variables(cls, llm_chain: LLMChain) -> LLMChain:
+        """Make sure the LLM chain has the correct input variables."""
+        if llm_chain.prompt.input_variables != ["query", "dialect"]:
+            raise ValueError("LLM chain for QueryCheckerTool must have input variables ['query', 'dialect']")
+        return llm_chain
+
+    def _run(self, query: str) -> str:
+        """Use the LLM to check the query."""
+        return self.llm_chain.predict(query=query, dialect=self.db.dialect)
+
+    async def _arun(self, query: str) -> str:
+        return await self.llm_chain.apredict(query=query, dialect=self.db.dialect)
