aab7ada8ca4f8b3d93cdf93d02d56a92faff684a
Brendan Turner <brendan.turner@heavy.ai>
2023-04-03 13:43:38 -0500

    Improve index and agent tools for lower token count; Change embedding function (#7)
    * update langchain
    
    * improve index and agent tools for lower token count
    
    * use huggingface sentence_transformers for creating embeddings

--- cli.py
+++ cli.py
@@ -73,6 +73,8 @@ def main(
         print(f"Prompt Tokens: {cb.prompt_tokens}")
         print(f"Completion Tokens: {cb.completion_tokens}")
         print(f"Total Tokens: {cb.total_tokens}")
+        print(f"Successful Requests: {cb.successful_requests}")
+        print(f"Total Cost (USD): ${cb.total_cost}")
 
 
 if __name__ == "__main__":
--- modules/langchain/tools/heavydb/tool.py
+++ modules/langchain/tools/heavydb/tool.py
@@ -1,11 +1,7 @@
-from langchain.chains.llm import LLMChain
-from langchain.llms.openai import OpenAI
-from langchain.prompts import PromptTemplate
 from langchain.tools.base import BaseTool
-from pydantic import BaseModel, Extra, Field, validator
+from pydantic import BaseModel, Extra, Field
 
 from modules.langchain import HeavyDB, heavydb_index
-from modules.langchain.tools.heavydb.prompts import QUERY_CHECKER
 
 
 class BaseHeavyDBTool(BaseModel):
@@ -27,17 +23,21 @@ class QueryHeavyDBSchemaTool(BaseHeavyDBTool, BaseTool):
 
     name = "query_sql_db_index"
     description = """
-    Use this tool to query an index for a HeavyDB database.
+    Use this tool to retrieve the schemas for a query.
 
     The input is a prompt that will be handled by a LLM with access to a HeavyDB index that contains schemas, sample rows, and summaries of each table in the database.
     Prepare a prompt that the LLM can use to fetch the information you require to complete your objective.
-    The output will be the result from the LLM responding to the prompt you provide.
-
-    Example prompt: "Which tables contain information about [topic]?"
+    The output will be schemas for the tables that the LLM thinks are relevant to the prompt.
     """
 
     def _run(self, prompt: str) -> str:
-        return heavydb_index.query(prompt)
+        resp = heavydb_index.query(
+            f"Please return the relevant table names (comma separated) for the following query: {prompt}"
+        )
+        table_names = resp.strip().split(", ")
+        if table_names == "I don't know.":
+            return "No information found. Please revise your prompt if you wish to try again."
+        return self.db.get_table_info(table_names)
 
     async def _arun(self, prompt: str) -> str:
         raise NotImplementedError("QueryHeavyDBSchemaTool does not support async")
@@ -48,13 +48,19 @@ class QueryHeavyDBTool(BaseHeavyDBTool, BaseTool):
 
     name = "query_sql_db"
     description = """
-    DO NOT USE THIS TOOL UNLESS YOU KNOW THE SCHEMA OF EACH TABLE YOU ARE QUERYING.
-    Use the schema_sql_db tool to obtain table schemas.
-    This tool is designed for querying a HeavyDB database.
+    ONLY USE WITH KNOWN TABLE SCHEMAS. This tool is for querying HeavyDB databases.
 
-    Provide a valid and accurate SQL query as input. The output will be the result from the database. Remember to apply a limit to your query.
-    In case the query is incorrect, an error message will be returned. If this occurs, revise the query, verify it, and try again.
-    Only use this tool if you are knowledgeable about the table schemas.
+    Input an accurate SQL query for output. Apply query limits. If an error occurs, revise and retry. Be familiar with table schemas and avoid common mistakes, such as:
+
+    - NULL values with NOT IN
+    - Using UNION instead of UNION ALL
+    - BETWEEN for exclusive ranges
+    - Data type mismatch
+    - Incorrectly quoting identifiers
+    - Wrong number of arguments in functions
+    - Incorrect data type casting
+    - Improper join columns
+    - Reserved SQL keywords as aliases
     """
 
     def _run(self, query: str) -> str:
@@ -100,90 +106,7 @@ class ListHeavyDBTool(BaseHeavyDBTool, BaseTool):
 
     def _run(self, _: str) -> str:
         """Get the list of tables in the database."""
-        return self.db.get_table_names()
+        return self.db.get_usable_table_names()
 
     async def _arun(self) -> str:
         raise NotImplementedError("ListSqlDbTool does not support async")
-
-
-class CanQuestionBeAnsweredHeavyDBTool(BaseHeavyDBTool, BaseTool):
-    """Tool for using an index to check if a question can be answered in a HeavyDB database."""
-
-    name = "can_question_be_answered_sql_db"
-    description = """
-    Use this tool to determine if a question can be answered. If the question can be answered, you may proceed to list_relevant_tables_sql_db.
-
-    Provide a question as input. The output will indicate if the question can be answered. If table names are included in the response, you can skip list_relevant_tables_sql_db and move on to schema_sql_db.
-    If the question cannot be answered, do not run list_relevant_tables_sql_db. In this case, you cannot answer the question and should not continue.
-    """
-
-    def _run(self, question: str) -> str:
-        query = f"Can information about the following prompt potentially be found in the database? If so which tables should be checked and or joined?: {question}"
-        res = heavydb_index.query(query)
-        return res
-
-    async def _arun(self) -> str:
-        raise NotImplementedError("CanQuestionBeAnsweredHeavyDBTool does not support async")
-
-
-class ListRelevantTablesHeavyDBTool(BaseHeavyDBTool, BaseTool):
-    """Tool for using an index to list relevant tables in a HeavyDB database."""
-
-    name = "list_relevant_tables_sql_db"
-    description = """
-    Ensure you have run can_question_be_answered_sql_db before using this tool. If not, do that first.
-    Provide a question as input. The output will indicate if the question can be answered and provide a list of tables in the database that may contain relevant information.
-    If the question cannot be answered, do not continue and state that the question cannot be answered.
-    """
-
-    def _run(self, question: str) -> str:
-        # retriever = heavydb_index.vectorstore.as_retriever()
-        # docs = retriever.get_relevant_documents(question)
-        # table_names = ", ".join(list(set([doc.metadata["source"] for doc in docs])))
-        # return table_names
-
-        # the following tried to answer the question using sources found outside of HeavyDB
-        # question: what is the population of alabama
-        # expected source: usa_states
-        # provided source: US Census Bureau
-        res = heavydb_index.query_with_sources(
-            f"Can information about the following prompt potentially be found in the database? If so which tables should be checked?: {question}"
-        )
-        table_names = ", ".join(list(set(res["sources"].split(", "))))
-        return f"Answer: {res['answer']}\nPotentially Relevant Tables: {table_names}"
-
-    async def _arun(self) -> str:
-        raise NotImplementedError("ListRelevantTablesHeavyDBTool does not support async")
-
-
-# This may need to be adapted to check HeavyDB's specific SQL dialect
-class QueryCheckerTool(BaseHeavyDBTool, BaseTool):
-    """Use an LLM to check if a query is correct.
-    Adapted from https://www.patterns.app/blog/2023/01/18/crunchbot-sql-analyst-gpt/"""
-
-    template: str = QUERY_CHECKER
-    llm_chain: LLMChain = Field(
-        default_factory=lambda: LLMChain(
-            llm=OpenAI(temperature=0),
-            prompt=PromptTemplate(template=QUERY_CHECKER, input_variables=["query", "dialect"]),
-        )
-    )
-    name = "query_checker_sql_db"
-    description = """
-    Use this tool to double check if your query is correct before executing it.
-    Always use this tool before executing a query with query_sql_db!
-    """
-
-    @validator("llm_chain")
-    def validate_llm_chain_input_variables(cls, llm_chain: LLMChain) -> LLMChain:
-        """Make sure the LLM chain has the correct input variables."""
-        if llm_chain.prompt.input_variables != ["query", "dialect"]:
-            raise ValueError("LLM chain for QueryCheckerTool must have input variables ['query', 'dialect']")
-        return llm_chain
-
-    def _run(self, query: str) -> str:
-        """Use the LLM to check the query."""
-        return self.llm_chain.predict(query=query, dialect=self.db.dialect)
-
-    async def _arun(self, query: str) -> str:
-        return await self.llm_chain.apredict(query=query, dialect=self.db.dialect)
