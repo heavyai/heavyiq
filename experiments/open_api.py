48e5dfdfd3b4e8fed424e625e425829809d004d9
Brendan Turner <bfturner@foglet.io>
2023-04-06 18:10:08 -0500

    forgot

---
+++ experiments/open_api.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+
+from langchain.tools import OpenAPISpec, APIOperation
+from langchain.chains import OpenAPIEndpointChain
+from langchain.requests import Requests
+from langchain.llms import OpenAI
+
+load_dotenv()
+
+
+def main():
+    spec = OpenAPISpec.from_url("http://localhost:5000/api/v1/openapi.json")
+    operation = APIOperation.from_openapi_spec(spec, "/query", "post")
+    chain = OpenAPIEndpointChain.from_api_operation(operation, OpenAI(), requests=Requests(), verbose=True)
+    chain.run("How many states in the usa_states table start with the letter A?")
+
+
+if __name__ == "__main__":
+    main()
--- modules/api/__init__.py
+++ modules/api/__init__.py
@@ -6,7 +6,10 @@ from flask_cors import CORS
 def get_app() -> Flask:
     app = connexion.FlaskApp(__name__)
     CORS(app.app)
-    app.add_api("heavyanalyst.yaml", strict_validation=True)
+
+    # for some reason the servers base url is not set to the full url, just "/api/v1"
+    # maybe it's set when deployed for production? in any case, must be resolved for LLM schema discovery
+    app.add_api("heavyanalyst.yaml", strict_validation=True, base_path="/api/v1")
 
     app.add_url_rule("/", "redirect_ui", lambda: redirect("/api/v1/ui/", code=302))
 
