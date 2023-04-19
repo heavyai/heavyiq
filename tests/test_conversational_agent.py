13f6676504b2ec13ac5d4407244f7931dceec5b0
Brendan Turner <brendan.turner@heavy.ai>
2023-04-19 15:23:02 -0500

    add a unit test (#26)

---
+++ tests/test_conversational_agent.py
@@ -0,0 +1,16 @@
+import unittest
+
+from langchain.agents import AgentExecutor
+
+from modules.langchain.agents.convo_agent import create_conversational_agent
+
+
+class TestConversationalAgent(unittest.TestCase):
+    def test_create_conversational_agent(self):
+        agent = create_conversational_agent()
+
+        self.assertIsInstance(agent, AgentExecutor)
+
+
+if __name__ == "__main__":
+    unittest.main()
