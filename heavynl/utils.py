dbb1e0739ca934b83dacfe3622be71b924e189f6
Brendan Turner <bfturner@foglet.io>
2023-05-03 14:50:06 -0500

    disallow destructive sql

--- heavynl/utils.py
+++ heavynl/utils.py
@@ -11,4 +11,24 @@ def strip_sql_comments(sql: str) -> str:
     # Remove leading and trailing double-quotes and single quotes
     sql = sql.strip("'\"")
     # Remove leading and trailing newlines
-    return re.sub(r"^\n+|\n+$", "", sql)
+    return re.sub(r"^\n+|\n+$", "", sql).strip()
+
+
+def is_destructive_sql(sql: str) -> bool:
+    """
+    Determines if a provided SQL statement is destructive or causes a modification.
+
+    Args:
+        sql (str): The SQL statement to be checked.
+
+    Returns:
+        bool: True if the SQL statement is destructive, False if it is non-destructive.
+    """
+    # Check for destructive SQL statements
+    destructive_statements = {"INSERT", "UPDATE", "DELETE", "DROP", "ALTER", "TRUNCATE", "REPLACE", "CREATE"}
+
+    # Convert the SQL statement to uppercase and split it by whitespace
+    sql_parts = sql.strip().upper().split()
+
+    # Check if the first part of the SQL statement is in the destructive_statements set
+    return sql_parts[0] in destructive_statements
