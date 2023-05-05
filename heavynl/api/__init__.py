e890f5b7b3dbdb3d1a672d8119131e8b2d95ec95
Brendan Turner <bfturner@foglet.io>
2023-05-05 15:39:48 -0500

    use create-react-app and serve it from flask

--- heavynl/api/__init__.py
+++ heavynl/api/__init__.py
@@ -1,7 +1,8 @@
 import os
+from typing import Any
 
 import connexion
-from flask import Flask, redirect
+from flask import Flask
 from flask_cors import CORS
 
 from heavynl.config import config
@@ -14,13 +15,16 @@ def get_app() -> Flask:
     # eagerly load the index
     get_heavydb_index()
 
-    app = connexion.FlaskApp(__name__)
+    # see heavynl/ui/README.md
+    app = connexion.FlaskApp(__name__, server_args={"static_folder": "../ui/build", "static_url_path": "/"})
     CORS(app.app)
+
+    @app.app.route("/")
+    def index() -> Any:
+        return app.app.send_static_file("index.html")
 
     # for some reason the servers base url is not set to the full url, just "/api/v1"
     # maybe it's set when deployed for production? in any case, must be resolved for LLM schema discovery
     app.add_api("heavyanalyst.yaml", strict_validation=True, base_path="/api/v1")
-
-    app.add_url_rule("/", "redirect_ui", lambda: redirect("/api/v1/ui/", code=302))
 
     return app.app
