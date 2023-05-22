1c9ee44f41b041f6ee071448026f3e13b6ba7930
Brendan Turner <bfturner@foglet.io>
2023-05-22 10:48:55 -0500

    add model/temperature options to CLI chain commands

--- heavynl/langchain/llms.py
+++ heavynl/langchain/llms.py
@@ -10,6 +10,13 @@ from heavynl.langchain.utils import is_promptlayer_active
 os.environ["OPENAI_API_KEY"] = config.openai_api_key
 
 
+def get_llm_by_model_name(model_name: str, tags: list[str] = [], **kwargs) -> OpenAI | ChatOpenAI:
+    if model_name.startswith("gpt-3.5") or model_name.startswith("gpt-4"):
+        return get_chat_llm(tags=tags, model_name=model_name, **kwargs)
+    else:
+        return get_llm(tags=tags, model_name=model_name, **kwargs)
+
+
 def get_llm(tags: list[str] = [], **kwargs) -> OpenAI:
     if is_promptlayer_active:
         return PromptLayerOpenAI(pl_tags=tags, return_pl_id=True, **kwargs)
