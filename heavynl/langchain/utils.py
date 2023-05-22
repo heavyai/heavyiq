b274b8585962ea0b36edd9a84be3c691ad725fe7
Brendan Turner <bfturner@foglet.io>
2023-05-22 09:40:27 -0500

    update non-langchain deps; add chain unit tests

--- heavynl/langchain/utils.py
+++ heavynl/langchain/utils.py
@@ -1,9 +1,12 @@
 import os
 
+import promptlayer
+
 from heavynl.config import config
 
 is_promptlayer_active = False
 
 if config.promptlayer_api_key is not None and config.promptlayer_api_key != "":
     os.environ["PROMPTLAYER_API_KEY"] = config.promptlayer_api_key
+    promptlayer.api_key = config.promptlayer_api_key
     is_promptlayer_active = True
---
+++ tests/test_chains.py
@@ -0,0 +1,27 @@
+import unittest
+
+from heavynl.langchain.chains import NLtoSQLChain, NLtoAnswerChain
+from heavynl.langchain.llms import get_llm
+from heavynl.langchain.logging import log_chain_call
+from heavynl.langchain import HeavyDB
+
+
+class TestChains(unittest.TestCase):
+    def test_nl_to_sql(self):
+        heavydb = HeavyDB.from_env(include_tables=["usa_states"])
+        llm = get_llm(["unit_test", "chain", "nl_to_sql_chain"], temperature=0.0, client=None)
+        chain = NLtoSQLChain(database=heavydb, llm=llm)
+        res = log_chain_call(chain, "How many states begin with the letter A? What are they?", "")
+        self.assertEqual("query" in res, True)
+        self.assertEqual("sql" in res, True)
+
+    def test_nl_to_answer(self):
+        heavydb = HeavyDB.from_env(include_tables=["usa_states"])
+        llm = get_llm(["unit_test", "chain", "nl_to_sql_chain"], temperature=0.0, client=None)
+        chain = NLtoAnswerChain(database=heavydb, llm=llm)
+        res = log_chain_call(chain, "How many states begin with the letter A? What are they?", "")
+        print(res)
+        self.assertEqual("query" in res, True)
+        self.assertEqual("sql" in res, True)
+        self.assertEqual("results" in res, True)
+        self.assertEqual("answer" in res, True)
