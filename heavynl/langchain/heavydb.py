3b95f25271c5e72eecb3c6e71895f9b087970b86
Avinash <avistylein3105@gmail.com>
2023-06-07 17:05:36 +0530

    redundant use heavydb.lock

--- heavynl/langchain/heavydb.py
+++ heavynl/langchain/heavydb.py
@@ -40,7 +40,7 @@ class HeavyDB:
         if self._ignore_tables:
             missing_tables = self._ignore_tables - self._all_tables
             if missing_tables:
-                print("WARNING: ignore_tables {missing_tables} not found in database")
+                print(f"WARNING: ignore_tables {missing_tables} not found in database")
 
         if not isinstance(sample_rows_in_table_info, int):
             raise TypeError("sample_rows_in_table_info must be an integer")
--- heavynl/langchain/index/generate_table_documents.py
+++ heavynl/langchain/index/generate_table_documents.py
@@ -32,9 +32,8 @@ def get_table_summary_document(heavydb: HeavyDB, table: str) -> Document:
     Returns:
         Document: A Document object containing the table summary and metadata.
     """
-    with heavydb.lock:
-        table_info = heavydb.get_table_info([table])
-    llm = get_chat_llm(["metadata_index", "table_summary"], model_name="gpt-4", temperature=0.2)
+    table_info = heavydb.get_table_info([table])
+    llm = get_chat_llm(["metadata_index", "table_summary"], model_name="gpt-3.5-turbo", temperature=0.2)
     messages = [
         SystemMessage(content=table_summary_prompt),
         HumanMessage(content=table_info),
@@ -62,8 +61,7 @@ def get_table_column_description_document(heavydb: HeavyDB, table: str) -> Docum
     Returns:
         Document: A Document object containing the column descriptions and metadata.
     """
-    with heavydb.lock:
-        table_info = heavydb.get_table_info([table])
+    table_info = heavydb.get_table_info([table])
     llm = get_chat_llm(["metadata_index", "table_columns_description"], model_name="gpt-3.5-turbo", temperature=0.2)
     messages = [
         SystemMessage(content=column_description_prompt),
