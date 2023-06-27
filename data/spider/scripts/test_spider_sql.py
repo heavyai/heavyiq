c6a9363610fb689de54922a5c88f7925edafbda7
Todd Mostak <tmostak@gmail.com>
2023-06-27 17:18:13 -0400

    test_spider_sql.py improvements, including auto-fixes for queries

--- data/spider/scripts/test_spider_sql.py
+++ data/spider/scripts/test_spider_sql.py
@@ -1,7 +1,9 @@
 import argparse
+import copy
 import csv
 import heavyai
 import json
+import openai
 import pandas as pd
 import re
 import sys
@@ -14,9 +16,14 @@ def getOptions(argv=None):
     parser.add_argument("-u", "--user", help="HeavyDB user name", default="admin")
     parser.add_argument("-w", "--password", help="HeavyDB password", default="HyperInteractive")
     parser.add_argument("-q", "--queries", help="Queries CSV file", default="./spider_qa.csv")
-    parser.add_argument("-d", "--database", help="HeavyDB database", default="None")
+    parser.add_argument("-d", "--database", help="HeavyDB database", default=None)
+    parser.add_argument("-k", "--openai-api-key", help="OpenAI API Key", default=None)
     parser.add_argument("--start-db", help="Start from this database idx", type=int, default=None)
     parser.add_argument("--num-dbs", help="Only process this many databases", type=int, default=0)
+    parser.add_argument(
+        "--fix-queries", help="Try to fix failed queries with automatically applied corrections", action="store_true"
+    )
+    parser.add_argument("--fix-queries-gpt", help="Try to fix failed queries with ChatGPT API", action="store_true")
     parser.add_argument("--write-prompts", help="Write prompts to file for successful queries", action="store_true")
     return parser.parse_args(argv)
 
@@ -84,13 +91,193 @@ def write_prompts_to_csv(prompts, output_file):
         writer.writerows(prompts_to_write)
 
 
+def extract_group_by_expressions(query):
+    # query = query.upper()
+    if "GROUP BY" not in query:
+        return []
+
+    # This pattern assumes that no other part of the query contains 'GROUP BY'
+    pattern = r"GROUP BY(.+?)( ORDER BY| HAVING|;|$)"
+    group_by_clause = re.search(pattern, query)
+
+    if group_by_clause:
+        # Separate multiple group by expressions by comma and strip whitespace
+        expressions = group_by_clause.group(1).split(",")
+        expressions = [expr.strip() for expr in expressions]
+        return expressions
+
+    return []
+
+
+def extract_projected_expressions(query):
+    # query = query.upper()
+    pattern = r"SELECT(.+?)FROM"
+    select_clause = re.search(pattern, query)
+
+    if select_clause:
+        # Separate multiple projected expressions by comma and strip whitespace
+        expressions = select_clause.group(1).split(",")
+        expressions = [expr.strip() for expr in expressions]
+        return expressions
+
+    return []
+
+
+def add_to_select_projection(query, new_items):
+    # query = query.upper()
+    pattern = r"SELECT(.+?)FROM"
+    select_clause = re.search(pattern, query)
+
+    if select_clause:
+        # Separate multiple projected expressions by comma and strip whitespace
+        expressions = select_clause.group(1).split(",")
+        expressions = [expr.strip() for expr in expressions]
+
+        # Add new items
+        expressions.extend(new_items)
+
+        # Reconstruct the SELECT clause
+        new_select_clause = ", ".join(expressions)
+
+        # Replace the original SELECT clause with the new one
+        query = re.sub(pattern, f"SELECT {new_select_clause} FROM", query)
+
+    return query
+
+
+def add_to_group_by(query, new_items):
+    # query = query.upper()
+    pattern = r"GROUP BY(.+?)( ORDER BY| HAVING|;|$)"
+    group_by_clause = re.search(pattern, query)
+
+    if group_by_clause:
+        # Separate multiple group by expressions by comma and strip whitespace
+        expressions = group_by_clause.group(1).split(",")
+        expressions = [expr.strip() for expr in expressions]
+
+        # Add new items
+        expressions.extend(new_items)
+
+        # Reconstruct the GROUP BY clause
+        new_group_by_clause = ", ".join(expressions)
+
+        # Replace the original GROUP BY clause with the new one
+        query = re.sub(pattern, f"GROUP BY {new_group_by_clause}\\2", query)
+
+    return query
+
+
+def fix_failed_query_unquoted_keyword(con, query_id, failed_query, error):
+    reserved_keywords = [
+        "YEAR",
+        "year",
+        "Year"
+        "DATE",
+        "date",
+        "Date"
+        "MONTH",
+        "month",
+        "Month"
+        "DAY",
+        "day",
+        "Day"
+        "RANK",
+        "rank",
+        "Rank"
+        "LENGTH",
+        "length",
+        "Length"
+    ]
+    sql_query = copy.deepcopy(failed_query)
+    for reserved_keyword in reserved_keywords:
+        if reserved_keyword in sql_query:
+            sql_query = sql_query.replace(reserved_keyword, f'"{reserved_keyword}"')
+    try:
+        sql_result = con.execute(sql_query)
+        return sql_query
+    except Exception as error:
+        return None
+
+
+def fix_failed_query_implicit_group_by(con, query_id, failed_query, error):
+    sql_query = copy.deepcopy(failed_query)
+    group_by_exprs = extract_group_by_expressions(sql_query)
+    if len(group_by_exprs) == 0:
+        return None
+    projected_exprs = extract_projected_expressions(sql_query)
+    if len(projected_exprs) == 0:
+        return None
+    group_by_exprs_to_add = []
+    agg_exprs = ["AVG", "COUNT", "MAX", "MIN", "SUM"]
+    for projected_expr in projected_exprs:
+        found_agg_expr = False
+        for agg_expr in agg_exprs:
+            if projected_expr.upper().startswith(agg_expr):
+                found_agg_expr = True
+                break
+        if found_agg_expr:
+            continue
+        if projected_expr not in group_by_exprs:
+            group_by_exprs_to_add.append(projected_expr)
+    if len(group_by_exprs_to_add) == 0:
+        return None
+    sql_query = add_to_group_by(sql_query, group_by_exprs_to_add)
+    try:
+        sql_result = con.execute(sql_query)
+        return sql_query
+    except Exception as error:
+        return None
+
+
+def fix_failed_query_gpt(con, query_id, failed_query, error):
+    sql = copy.deepcopy(failed_query)
+    prompt = f"""When translating the following SQL query to work in the OmniSciDB database, the following error was received.\n{error}\n\n.Note that OmniSciDB has the following limitations:\n1.INTERSECT, EXCEPT, and UNION claues are not supported (UNION ALL is supported, however).\n2.Database reserved keywords like "YEAR", "MONTH", "DAY", "RANK" and "LENGTH" need to be enclosed in double-quotes.\n3.Implicit group-by is not supported, meaning that all projected columns in a group-by query must either be in the GROUP BY clause or be aggregates.\n3. Columns of any numeric type cannot be joined with columns of type TEXt. To join these columns, either the numeric column must be casted to TEXT via a CAST operator, or the TEXT column casted to a numeric type with TRY_CAST.\n\nWith these limitations in mind, please alter the following query to run in OmniSciDB: """
+
+    max_sql_try_count = 2
+    sql_try_count = 1
+    sql_success = False
+    modified_prompt = copy.deepcopy(prompt)
+    base_model = "gpt-3.5-turbo"
+    power_model = "gpt-4"
+    temperature = 0.05
+    while sql_try_count <= max_sql_try_count and not sql_success:
+        sql_completion = None
+        model = base_model if sql_try_count == 1 else power_model
+        print(sql_try_count)
+        try:
+            sql_completion = openai.ChatCompletion.create(
+                model=model,
+                messages=[{"role": "system", "content": modified_prompt}, {"role": "user", "content": sql}],
+                temperature=temperature,
+            )
+            sql = sql_completion["choices"][0]["message"]["content"].replace("\n", " ").replace("\r", " ")
+            sql = re.sub(".*(SELECT.*;).*", r"\1", sql, count=0, flags=0)
+            # print("""SQL {sql_try_count}: {sql}""".format(sql_try_count=sql_try_count, sql=sql))
+            sql_result = con.execute(sql)
+            sql_try_count += 1
+            sql_success = True
+        except Exception as error:
+            # print(error)
+            prompt = f"""When translating the following SQL query to work in the OmniSciDB database, the following error was received.\n{error}\n\n.Note that OmniSciDB has the following limitations:\n1.INTERSECT, EXCEPT, and UNION claues are not supported (UNION ALL is supported, however).\n2.Database reserved keywords like "YEAR", "MONTH", "DAY", "RANK" and "LENGTH" need to be enclosed in double-quotes.\n3.Implicit group-by is not supported, meaning that all projected columns in a group-by query must either be in the GROUP BY clause or be aggregates.\n3. Columns of any numeric type cannot be joined with columns of type TEXt. To join these columns, either the numeric column must be casted to TEXT via a CAST operator, or the TEXT column casted to a numeric type with TRY_CAST.\n\nWith these limitations in mind, please alter the following query to run in OmniSciDB: """
+            sql_try_count += 1
+            continue
+    if not sql_success:
+        print(f"Query {query_id} FAILED: {failed_query}")
+        return None
+    else:
+        print(f"Query {query_id} FIXED: {sql}")
+        return sql
+
+
 def main(argv):
     options = getOptions(argv)
+    openai.api_key = options.openai_api_key
     queries_by_db = getQueriesByDB(options.queries)
     db_num = 0
     total_queries = 0
     total_successful_queries = 0
     total_successful_altered_queries = 0
+    fixed_queries = []
     failed_queries = []
     prompts = []
     con = None
@@ -130,19 +317,36 @@ def main(argv):
                         # sql_query_with_semicolon = sql_query + ";" if sql_query[-1] != ";" else sql_query
                         prompts.append({"instruction": instruction, "input": "", "output": sql_query})
                 except Exception as e:
-                    if '"' in sql_query:
-                        sql_query = sql_query.replace('"', "'")
-                        try:
-                            results = list(con.execute(sql_query))
-                            successful_queries += 1
-                            total_successful_altered_queries += 1
-                        except Exception as e:
-                            failed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": sql_query})
-                            continue
-                    else:
-                        # failed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": sql_query, "error": e})
+                    query_fixed = False
+                    if options.fix_queries:
+                        fixed_query = fix_failed_query_unquoted_keyword(con, query_id, sql_query, e)
+                        if fixed_query is None:
+                            fixed_query = fix_failed_query_implicit_group_by(con, query_id, sql_query, e)
+                        if fixed_query is not None:
+                            fixed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": fixed_query})
+                            query_fixed = True
+                    if options.fix_queries_gpt and not query_fixed:
+                        fixed_query = fix_failed_query_gpt(con, query_id, sql_query, e)
+                        if fixed_query is not None:
+                            # print(f"Fixed query: {fixed_query}")
+                            fixed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": fixed_query})
+                            query_fixed = True
+                    if not query_fixed:
                         failed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": sql_query})
-                        continue
+
+                    # if '"' in sql_query:
+                    #    sql_query = sql_query.replace('"', "'")
+                    #    try:
+                    #        results = list(con.execute(sql_query))
+                    #        successful_queries += 1
+                    #        total_successful_altered_queries += 1
+                    #    except Exception as e:
+                    #        failed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": sql_query})
+                    #        continue
+                    # else:
+                    #    # failed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": sql_query, "error": e})
+                    #    failed_queries.append({"query_id": query_id, "db_id": db_id, "sql_query": sql_query})
+                    #    continue
             total_queries += num_queries
             total_successful_queries += successful_queries
             print(f"{db_num}: {db_id} successful queries: {successful_queries}/{num_queries}")
@@ -153,12 +357,16 @@ def main(argv):
             pass
     failures_df = pd.DataFrame(failed_queries)
     failures_df.to_csv("failed_queries.csv", index=False)
+    if options.fix_queries or options.fix_queries_gpt:
+        fixes_df = pd.DataFrame(fixed_queries)
+        fixes_df.to_csv("fixed_queries.csv", index=False)
     if options.write_prompts:
         with open("sql_training_prompts.json", "w") as file:
             json.dump(prompts, file, indent=4)
         write_prompts_to_csv(prompts, "sql_training_prompts.csv")
     print(f"\n\nTotal successful queries: {total_successful_queries}/{total_queries}")
-    print(f"Total successful altered queries: {total_successful_altered_queries}/{total_queries}")
+    print(f"Total successful fixed queries: {len(fixed_queries)}/{total_queries}")
+    # print(f"Total successful altered queries: {total_successful_altered_queries}/{total_queries}")
 
 
 if __name__ == "__main__":
