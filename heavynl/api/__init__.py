48b8a083e1c616e356cd4683a46d012a2cbb9815
Brendan Turner <bfturner@foglet.io>
2023-06-27 13:02:43 -0500

    change config block names to [iq]|[iq.*]; rename heavyanalyst.yaml -> heavyiq-spec.yaml

--- heavynl/api/__init__.py
+++ heavynl/api/__init__.py
@@ -17,7 +17,7 @@ def get_app(config_path: str = "./config.toml") -> Flask:
 
     # for some reason the servers base url is not set to the full url, just "/api/v1"
     # maybe it's set when deployed for production? in any case, must be resolved for LLM schema discovery
-    app.add_api("heavyanalyst.yaml", strict_validation=True, base_path="/api/v1")
+    app.add_api("heavyiq-spec.yaml", strict_validation=True, base_path="/api/v1")
 
     app.add_url_rule("/", "redirect_ui", lambda: redirect("/api/v1/ui/", code=302))
 
