2c78cf8b6eebd74a5ba9beacf3d6f104a347801a
Todd Mostak <tmostak@gmail.com>
2023-06-27 18:27:35 -0400

    eval_sql.py: WIP 2/?

--- data/spider/scripts/eval_sql.py
+++ data/spider/scripts/eval_sql.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import heavyai
 import sys
 
@@ -47,10 +48,27 @@ def getOptions(argv=None):
     return parser.parse_args(argv)
 
 
+def writeQueryResultsCsv(filename, query_results):
+    # Specify the fieldnames (headers of the CSV)
+    fieldnames = ["query_id", "db_id", "question", "gold_sql_query", "gen_sql_query", "gold_run_success", "gen_result"]
+
+    # Write the dictionaries to a CSV file
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write the header row
+        writer.writeheader()
+
+        # Write all query results
+        for query_result in query_results:
+            writer.writerow(query_result)
+
+
 def main(argv):
     options = getOptions(argv)
     queries_by_db = getQueriesByDB(options.queries)
     con = None
+    query_results = []
     for db_id, queries in queries_by_db.items():
         try:
             con = heavyai.connect(user=options.user, password=options.password, host=options.host, dbname=db_id)
@@ -60,20 +78,37 @@ def main(argv):
         num_queries = len(queries)
         for query in queries:
             query_id = query["query_id"]
+            question = query["question"]
             gold_sql_query = query["gold_sql_query"]
             gen_sql_query = query["gen_sql_query"]
             gold_result = None
             gen_result = None
+            gold_run_success = False
+            gen_run_success = False
             try:
                 gold_result = con.select_ipc(gold_sql_query)
+                gold_run_success = True
             except Exception as e:
                 print(f"Error executing gold query {query_id} on database {db_id}: {e}")
                 continue
             try:
                 gen_result = con.select_ipc(gen_sql_query)
+                gen_run_success = True
             except Exception as e:
                 print(f"Error executing generated query {query_id} on database {db_id}: {e}")
                 continue
+            query_results.append(
+                {
+                    "query_id": query_id,
+                    "db_id": db_id,
+                    "question": question,
+                    "gold_sql_query": gold_sql_query,
+                    "gen_sql_query": gen_sql_query,
+                    "gold_run_success": gold_run_success,
+                    "gen_result": gen_run_success,
+                }
+            )
+    writeQueryResultsCsv("eval_results.csv", query_results)
 
 
 if __name__ == "__main__":
