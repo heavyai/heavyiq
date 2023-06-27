86cb454eb48332b1a31decd237b70ada3cb0b333
Brendan Turner <bfturner@foglet.io>
2023-06-27 14:16:14 -0500

    dont init logs until after config_path is set

--- heavynl/api/handlers/__init__.py
+++ heavynl/api/handlers/__init__.py
@@ -1,6 +1,6 @@
 from heavynl.api.utils import handle_errors
 from heavynl.config import get_config
-from heavynl.logging_utils import heavynl_logger as logger
+from heavynl.logging_utils import get_heavynl_logger
 from heavynl.langchain import HeavyDB
 from heavynl.langchain.chains import NLtoAnswerChain, get_nl_to_sql_chain_by_llm
 from heavynl.langchain.llms import get_llm_by_model_name
@@ -13,6 +13,7 @@ MODEL_NAME = get_config().openai_gpt_model
 
 @handle_errors
 def query(body: dict) -> dict:
+    logger = get_heavynl_logger()
     db_session_id = body["session_id"]
     tables = body["tables"]
     db = HeavyDB.from_session(db_session_id, include_tables=tables)
@@ -32,6 +33,7 @@ def query(body: dict) -> dict:
 
 @handle_errors
 def question(body: dict) -> dict:
+    logger = get_heavynl_logger()
     db_session_id = body["session_id"]
     tables = body["tables"]
     db = HeavyDB.from_session(db_session_id, include_tables=tables)
