300dac6feffe75a372da92f4663d73aaf279f4e7
Todd Mostak <tmostak@gmail.com>
2023-07-07 12:36:08 -0400

    Update gen_sql.py and eval_sql.py

--- data/spider/scripts/eval_sql.py
+++ data/spider/scripts/eval_sql.py
@@ -1,39 +1,20 @@
 import argparse
 import csv
 import heavyai
+import json
 import sys
 
-
-def getQueriesByDB(queries_file):
-    queries_df = pd.read_csv(queries_file)
-    queries_df["question"] = queries_df["question"].astype(str)
-    queries_df["gold_sql_query"] = queries_df["gold_sql_query"].astype(str)
-    queries_df["gen_sql_query"] = queries_df["gen_sql_query"].astype(str)
+def getQueriesByDB(prompts_filename):
+    prompts = []
+    with open(prompts_filename, "r") as f:
+        prompts = json.load(f)
     queries_by_db = {}
-    for index, row in queries_df.iterrows():
-        query_id = row["query_id"]
-        db_id = row["db_id"]
-        gold_sql_query = row["gold_sql_query"]
-        gen_sql_query = row["gen_sql_query"]
-        question = row["question"]
-        if db_id not in queries_by_db:
-            queries_by_db[db_id] = [
-                {
-                    "query_id": query_id,
-                    "question": question,
-                    "gold_sql_query": gold_sql_query,
-                    "gen_sql_query": gen_sql_query,
-                }
-            ]
-        else:
-            queries_by_db[db_id].append(
-                {
-                    "query_id": query_id,
-                    "question": question,
-                    "gold_sql_query": gold_sql_query,
-                    "gen_sql_query": gen_sql_query,
-                }
-            )
+    for prompt in prompts:
+         db_id = prompt["db_id"]
+         if db_id not in queries_by_db:
+             queries_by_db[db_id] = [prompt]
+         else:
+             queries_by_db[db_id].append(prompt)
     return queries_by_db
 
 
@@ -43,14 +24,14 @@ def getOptions(argv=None):
     parser.add_argument("-p", "--port", help="HeavyDB server port", default="6273")
     parser.add_argument("-u", "--user", help="HeavyDB user name", default="admin")
     parser.add_argument("-w", "--password", help="HeavyDB password", default="HyperInteractive")
-    parser.add_argument("--queries", help="Test queries CSV file")
+    parser.add_argument("--prompts", help="Test prompts JSON file")
     # parser.add_argument("--gold-queries", help="Gold eval queries CSV file")
     return parser.parse_args(argv)
 
 
 def writeQueryResultsCsv(filename, query_results):
     # Specify the fieldnames (headers of the CSV)
-    fieldnames = ["query_id", "db_id", "question", "gold_sql_query", "gen_sql_query", "gold_run_success", "gen_result"]
+    fieldnames = ["query_id", "db_id", "prompt", "gold_sql_query", "gen_sql_query", "gold_run_success", "gen_run_success"]
 
     # Write the dictionaries to a CSV file
     with open(filename, "w", newline="") as csvfile:
@@ -66,10 +47,14 @@ def writeQueryResultsCsv(filename, query_results):
 
 def main(argv):
     options = getOptions(argv)
-    queries_by_db = getQueriesByDB(options.queries)
+    queries_by_db = getQueriesByDB(options.prompts)
     con = None
     query_results = []
+    query_idx = 0
+    max_queries = 700
     for db_id, queries in queries_by_db.items():
+        if query_idx >= max_queries:
+            break
         try:
             con = heavyai.connect(user=options.user, password=options.password, host=options.host, dbname=db_id)
         except Exception as e:
@@ -77,8 +62,10 @@ def main(argv):
             continue
         num_queries = len(queries)
         for query in queries:
+            if query_idx >= max_queries:
+                break
             query_id = query["query_id"]
-            question = query["question"]
+            prompt = query["prompt"]
             gold_sql_query = query["gold_sql_query"]
             gen_sql_query = query["gen_sql_query"]
             gold_result = None
@@ -86,28 +73,28 @@ def main(argv):
             gold_run_success = False
             gen_run_success = False
             try:
-                gold_result = con.select_ipc(gold_sql_query)
+                gold_result = con.execute(gold_sql_query)
                 gold_run_success = True
             except Exception as e:
                 print(f"Error executing gold query {query_id} on database {db_id}: {e}")
-                continue
             try:
-                gen_result = con.select_ipc(gen_sql_query)
+                gen_result = con.execute(gen_sql_query)
                 gen_run_success = True
             except Exception as e:
                 print(f"Error executing generated query {query_id} on database {db_id}: {e}")
-                continue
             query_results.append(
                 {
                     "query_id": query_id,
                     "db_id": db_id,
-                    "question": question,
+                    "prompt": prompt,
                     "gold_sql_query": gold_sql_query,
                     "gen_sql_query": gen_sql_query,
                     "gold_run_success": gold_run_success,
-                    "gen_result": gen_run_success,
+                    "gen_run_success": gen_run_success,
                 }
             )
+            print(query_idx)
+            query_idx += 1
     writeQueryResultsCsv("eval_results.csv", query_results)
 
 
--- data/spider/scripts/gen_sql.py
+++ data/spider/scripts/gen_sql.py
@@ -11,6 +11,7 @@ def getOptions(argv=None):
     parser.add_argument("-m", "--model", help="Path to model")
     parser.add_argument("--prefix", help="Optional prefix for prompt", default=None)
     parser.add_argument("--suffix", help="Optional suffix for prompt", default=None)
+    parser.add_argument("-g", "--gpu", help="Use CUDA GPU", action="store_true")
     return parser.parse_args(argv)
 
 
@@ -21,33 +22,34 @@ def getPrompts(prompts_filename):
 
 
 def gen_completion(prompt, model):
-    completion = model(prompt, max_tokens=200, stop=[";"])
+    completion = model(prompt, max_tokens=200, temperature=0.0, stop=[";"])
     return completion
 
 
 def main(argv):
     options = getOptions(argv)
     prompts = getPrompts(options.prompts)
-    model = Llama(model_path=options.model)
+    model = None
+    num_gpu_layers = 100 if options.gpu else 0
+    model = Llama(model_path=options.model, n_gpu_layers=num_gpu_layers, n_ctx=1280)
     eval_outputs = []
     for idx, prompt_example in enumerate(prompts):
+        print(idx)
         prompt = prompt_example["instruction"]
         if options.prefix is not None:
             prompt = options.prefix + prompt
         if options.suffix is not None:
-            prompt = prompt + options.suffix 
+            prompt = prompt + options.suffix
         completion = gen_completion(prompt, model)
         response = completion["choices"][0]["text"]
-        eval_output = {"query_id": prompt_example["query_id"], "prompt": prompt, "gold_sql_query": prompt_example["output"]}
-        sql = re.sub(".*(SELECT.*;).*", r"\1", response, count=0, flags=0)
+        eval_output = {"query_id": prompt_example["query_id"], "db_id": prompt_example["db_id"], "prompt": prompt, "gold_sql_query": prompt_example["output"]}
+        sql = re.sub(".*(SELECT.*;).*", r"\1", response, count=0, flags=0) + ";"
         if sql is not None:
             print(sql)
             eval_output["gen_sql_query"] = sql
         else:
             eval_output["gen_sql_query"] = None
         eval_outputs.append(eval_output)
-        if idx > 10:
-            break
     with open("eval_queries.json", "w") as file:
         json.dump(eval_outputs, file, indent=4)
 
@@ -55,4 +57,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
