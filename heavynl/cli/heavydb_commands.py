03ab4354dcd1bd5e4b39b6e27ca03ad59059a2b2
Avinash <avistylein3105@gmail.com>
2023-07-07 07:56:33 +0530

    support for making db connection persistant

--- heavynl/cli/heavydb_commands.py
+++ heavynl/cli/heavydb_commands.py
@@ -12,6 +12,11 @@ def db():
 @click.option("--db-name", default="", help="HeavyDB database to connect with.", type=str)
 @click.pass_context
 def session(ctx: click.Context, db_name: str) -> None:
-    """Ask the HeavyDB to create and return the session id."""
+    """
+    Ask the HeavyDB to establish a new db connection and return it's corresponding session id.
+
+    Note: Don't call this command again and again. Since the session id will expire after an hour,
+    you can re-use the previously generated session id's on endpoint calls until it gets timed-out.
+    """
     heavydb_sessionid = HeavyDB.create_session_id(db_name=db_name)
     click.echo(heavydb_sessionid, color=True)
