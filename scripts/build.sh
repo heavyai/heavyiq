9fe524c09251bd98069b533c724b34774cb3265d
Brendan Turner <bfturner@foglet.io>
2023-07-13 13:17:01 -0500

    add build script and static route to serve version.txt

---
+++ scripts/build.sh
@@ -0,0 +1,25 @@
+# ENV Requirements
+## python3.10 (Installed)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jenkins/tmp/Python-3.10.12
+
+# Create New Virtual Environment
+python3.10 -m venv venv
+. venv/bin/activate
+
+# Install Requirements
+pip install -r requirements.txt -r requirements-dev.txt
+
+# Create Obfuscated Build
+pyarmor reg pyarmor-regfile-5130.zip
+pyarmor gen ./heavynl
+cp requirements.txt ./dist/requirements.txt
+cp heavynl/api/heavyiq-spec.yaml ./dist/heavynl/api/heavyiq-spec.yaml
+
+# Create version.txt
+mkdir -p dist/public
+current_timestamp=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+commit_hash=$(git rev-parse --short HEAD)
+echo "${current_timestamp}-${commit_hash}" > dist/public/version.txt
+
+# Compress Build Dir
+tar -czf dist.tgz dist
