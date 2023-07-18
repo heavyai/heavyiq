990fc062f9668a6b1159016392a2f9d4fa16f26a
Brendan Turner <bfturner@foglet.io>
2023-07-18 08:21:22 -0500

    disable license check temporarily

--- heavynl/config/__init__.py
+++ heavynl/config/__init__.py
@@ -89,6 +89,7 @@ def get_config(file: str = "./config.toml") -> HeavyNLConfig:
         os.makedirs(app_config.iq.data)
     _config = app_config.iq
 
-    get_heavydb_license_claims(_config)
+    # disabled for now because HeavyIQ can startup before HeavyDB
+    # get_heavydb_license_claims(_config)
 
     return _config
