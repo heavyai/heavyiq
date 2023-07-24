460d1f7f45b0205393389e2b95df499b3857983e
Brendan Turner <bfturner@foglet.io>
2023-07-24 12:42:03 -0500

    [AI-73] change api error handler to extract error message from HeavyNLBaseException

--- heavynl/api/utils.py
+++ heavynl/api/utils.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from typing import Any, Callable
 
+from heavynl.langchain.exceptions import HeavyNLBaseException
 from heavynl.logging_utils import get_heavynl_logger
 
 
@@ -10,6 +11,9 @@ def handle_errors(func: Callable[..., Any]) -> Callable[..., tuple[Any | dict, i
         logger = get_heavynl_logger()
         try:
             return func(*args, **kwargs), 200
+        except HeavyNLBaseException as e:
+            logger.exception(str(e))
+            return {"error": e.message}, 500
         except Exception as e:
             logger.exception(str(e))
             return {"error": str(e)}, 500
