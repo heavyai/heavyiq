637882794804dc2be1df0048a392dbd02c99c03b
Brendan Turner <bfturner@foglet.io>
2023-07-26 09:44:55 -0500

    init promptlayer to avoid get_config in toplayer

--- heavynl/cli/__init__.py
+++ heavynl/cli/__init__.py
@@ -1,10 +1,14 @@
 import click
 
+from heavynl.langchain.utils import init_promptlayer
+
 from .agent_commands import agent
 from .chain_commands import chain
 from .eval_commands import eval
 from .metadata_index_commands import metadata_index
 from .heavydb_commands import db
+
+init_promptlayer()
 
 
 @click.group()
