0219917b6c3c81e422edc58b533f01f926f6436c
Brendan Turner <brendan.turner@heavy.ai>
2023-08-29 10:52:09 -0500

    [AI-86] support vllm api (#102)
    * support vllm api
    
    * accommodate API_VLLM in config validation
    
    * quickfix
    
    * set use_beam_search on kwargs for API_VLLM
    
    * set use_beam_search on model_kwargs (best_of on kwargs) for API_VLLM
    
    * use custom prompt for API_VLLM
    
    * only use beam search for nl-to-sql; fetch vllm model name
    
    * quickfix
    
    * make helper for determining if custom trained llm is used
    
    * update config
    
    * update deps

--- heavyiq/config/config_schema.py
+++ heavyiq/config/config_schema.py
@@ -39,8 +39,9 @@ class HeavyIQConfig(OverrideBaseConfig):  # type: ignore
     log_to_stdout: bool = False
     # CUSTOM LLM
     custom_llm_type: Optional[str] = None
-    """ 'API' or 'AZURE' """
+    """ 'API' or 'API_VLLM' or 'AZURE' """
     custom_llm_api_base: str = ""
+    custom_llm_api_vllm_beam_width: int = 1
     custom_llm_api_context_window: int = 2048
     custom_llm_azure_openai_api_version: str = "2023-03-15-preview"
     custom_llm_azure_openai_api_base: str = ""
--- heavyiq/langchain/index/generate_table_documents.py
+++ heavyiq/langchain/index/generate_table_documents.py
@@ -1,3 +1,4 @@
+import os
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 
