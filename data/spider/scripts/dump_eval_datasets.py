6c2d2a4a3856903cf62199e4d08b7bde15884b46
Todd Mostak <tmostak@gmail.com>
2023-09-12 17:25:42 -0400

    Add dump and restore eval datasets scripts

---
+++ data/spider/scripts/dump_eval_datasets.py
@@ -0,0 +1,40 @@
+import heavyai
+import pathlib
+
+eval_databases = [
+    "concert_singer",
+    "pets_1",
+    "employee_hire_evaluation",
+    "cre_Doc_Template_Mgt",
+    "course_teach",
+    "museum_visit",
+    "battle_death",
+    "student_transcripts_tracking",
+    "tvshow",
+    "poker_player",
+    "voter_1",
+    "world_1",
+    "orchestra",
+    "network_1",
+    "dog_kennels",
+    "singer",
+    "real_estate_properties",
+]
+
+local_export_base_dir = "/Users/todd//data/heavyai_dumps/heavyiq/eval"
+export_base_dir = "/media/psf/Home/data/heavyai_dumps/heavyiq/eval"
+for db in eval_databases:
+    print(f"Database: {db}")
+    con = heavyai.connect(user="admin", password="HyperInteractive", host="10.211.55.4", dbname=db)
+    tables = con.get_tables()
+    local_export_db_path = f"{local_export_base_dir}/{db}"
+    export_db_path = f"{export_base_dir}/{db}"
+    pathlib.Path(local_export_db_path).mkdir(parents=True, exist_ok=True)
+    for table in tables:
+        print(f"Table: {table}")
+        export_path = f"{export_db_path}/{table}.dump.lz4"
+        dump_table_sql = f"DUMP TABLE {table} TO '{export_path}'"
+        print(dump_table_sql)
+        con.execute(dump_table_sql)
+
+    print(tables)
---
+++ data/spider/scripts/restore_eval_datasets.py
@@ -0,0 +1,44 @@
+mport heavyai
+import pathlib
+
+eval_databases = [
+    "concert_singer",
+    "pets_1",
+    "employee_hire_evaluation",
+    "cre_Doc_Template_Mgt",
+    "course_teach",
+    "museum_visit",
+    "battle_death",
+    "student_transcripts_tracking",
+    "tvshow",
+    "poker_player",
+    "voter_1",
+    "world_1",
+    "orchestra",
+    "network_1",
+    "dog_kennels",
+    "singer",
+    "real_estate_properties",
+]
+
+local_import_base_dir = "/home/paperspace/heavyiq_eval_dumps"
+heavyai_con = heavyai.connect(user="admin", password="HyperInteractive", host="localhost", dbname="heavyai")
+for db in eval_databases:
+    create_db_sql = f"CREATE DATABASE IF NOT EXISTS {db}"
+    heavyai_con.execute(create_db_sql)
+
+for db in eval_databases:
+    print(f"Database: {db}")
+    con = heavyai.connect(user="admin", password="HyperInteractive", host="localhost", dbname=db)
+    local_import_dir = f"{local_import_base_dir}/{db}"
+    dump_files = pathlib.Path(local_import_dir).glob("*.dump.lz4")
+    print(dump_files)
+    tables = [str(dump_file).split('.')[0].split('/')[-1] for dump_file in dump_files]
+    for table in tables:
+        print(f"Table: {table}")
+        restore_sql = f"RESTORE TABLE {table} FROM '{local_import_dir}/{table}.dump.lz4'"
+        print(restore_sql)
+        try:
+            con.execute(restore_sql)
+        except Exception as e:
+            print(e)
