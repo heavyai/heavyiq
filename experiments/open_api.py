4d169ebb61b30797bf8976eb13cb9eb74f05b476
Avinash <avistylein3105@gmail.com>
2023-09-13 22:31:04 +0530

    Updated experimental openapi script which answers questions by passing openapi spec to the llm

--- experiments/open_api.py
+++ experiments/open_api.py
@@ -1,20 +1,40 @@
 import os
+import requests
+import json
+import sys
+from typing import Any
+from langchain.agents.agent_toolkits.openapi import planner
+from langchain.agents.agent_toolkits.openapi.spec import reduce_openapi_spec, ReducedOpenAPISpec
+from langchain.requests import RequestsWrapper
 
-from langchain.tools import OpenAPISpec, APIOperation
-from langchain.chains import OpenAPIEndpointChain
-from langchain.requests import Requests
-from langchain.llms import OpenAI
+# append the current directory to the sys path, so that you could run this script
+# directly from the root folder like,
+# python experiments/open_api.py
+sys.path.append(".")
 
 from heavyiq.config import get_config
+from heavyiq.langchain.llms import get_chat_llm
 
 os.environ["OPENAI_API_KEY"] = get_config().openai_api_key
 
 
+def get_spec(spec_path="http://localhost:8000/openapi.json") -> ReducedOpenAPISpec:
+    """
+    return the spec dict from swagger url.
+    """
+    response = requests.get(spec_path, verify=False)
+    spec_dict = json.loads(response.content)
+    return reduce_openapi_spec(spec_dict)
+
+
 def main():
-    spec = OpenAPISpec.from_url("http://localhost:5000/api/v1/openapi.json")
-    operation = APIOperation.from_openapi_spec(spec, "/query", "post")
-    chain = OpenAPIEndpointChain.from_api_operation(operation, OpenAI(), requests=Requests(), verbose=True)
-    chain.run("How many states in the usa_states table start with the letter A?")
+    requests_wrapper = RequestsWrapper()
+    model = get_config().openai_gpt_model
+    llm = get_chat_llm(model=model)
+
+    heavyiq_agent = planner.create_openapi_agent(get_spec(), requests_wrapper, llm)
+    user_query = "How many states in the usa_states table start with the letter A?"
+    heavyiq_agent.run(user_query)
 
 
 if __name__ == "__main__":
--- heavyiq/api/__init__.py
+++ heavyiq/api/__init__.py
@@ -41,7 +41,7 @@ def create_app(config_path: str = "./config.toml") -> FastAPI:
     init_logs()  # initializes logs using config
     init_telemetrics()  # initializes langsmith
 
-    app = FastAPI(title="HeavyIQ")
+    app = FastAPI(title="HeavyIQ", servers=[{"url": "http://localhost:8000", "description": "Local server"}])
 
     cors_origins = ["http://localhost"]
 
@@ -123,6 +123,8 @@ def create_app(config_path: str = "./config.toml") -> FastAPI:
             summary="Endpoints for doing NL operations.",
             description="",
             routes=app.routes,
+            tags=app.openapi_tags,
+            servers=app.servers,
         )
 
         # hard way to remove certain schemas from default openapi schema
