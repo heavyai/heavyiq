0c89d1dff374a1873dac7951a179c49c1bc7a37e
Avinash Raj <avistylein3105@gmail.com>
2023-09-14 20:00:01 +0530

    removed synchronous handler functions (#118)

--- heavyiq/api/handlers/__init__.py
+++ heavyiq/api/handlers/__init__.py
@@ -1,6 +1,4 @@
 from .iq_handler import (
-    handle_query_request,
-    handle_question_request,
     handle_query_request_async,
     handle_question_request_async,
     handle_submit_feedback_request_async,
--- heavyiq/api/handlers/iq_handler.py
+++ heavyiq/api/handlers/iq_handler.py
@@ -22,32 +22,8 @@ from heavyiq.langchain.chains import (
     get_ask_docs_chain,
 )
 from heavyiq.langchain.llms import LLMType, get_llm_by_type
-from heavyiq.langchain.logging import log_chain_call, log_chain_call_async
+from heavyiq.langchain.logging import log_chain_call_async
 from heavyiq.logging_utils import get_heavyiq_logger
-
-
-def handle_query_request(request: QueryRequest) -> QueryResponse:
-    """
-    Handles /query request.
-
-    Args:
-        request (QueryRequest): request payload
-
-    Returns:
-        QueryResponse: response content
-    """
-    tables = request.tables
-    db = HeavyDB.from_session(request.session_id, include_tables=tables)
-    question = request.question
-    llm = get_llm_by_type(LLMType.NL_TO_SQL, temperature=0.0)
-    chain_cls = get_nl_to_sql_chain_by_llm(llm=llm)
-    chain = chain_cls(llm=llm, database=db, tags=["rest-api", "query-endpoint"])  # type: ignore
-    chain_input = {chain.input_key: question, "tables": tables}
-    res = log_chain_call(chain, chain_input, "")
-    feedback_id = str(res["__run"].run_id) if "__run" in res else ""
-    return QueryResponse(
-        sql=res[chain.output_key], sql_complexity=res[chain.output_complexity_key], feedback_id=feedback_id
-    )
 
 
 async def handle_query_request_async(request: QueryRequest, db: HeavyDB) -> QueryResponse:
@@ -71,42 +47,6 @@ async def handle_query_request_async(request: QueryRequest, db: HeavyDB) -> Quer
     feedback_id = str(res["__run"].run_id) if "__run" in res else ""
     return QueryResponse(
         sql=res[chain.output_key], sql_complexity=res[chain.output_complexity_key], feedback_id=feedback_id
-    )
-
-
-def handle_question_request(request: QuestionRequest) -> QuestionResponse:
-    """
-    Handles /question request.
-
-    Args:
-        request (QuestionRequest): request payload
-
-    Returns:
-        QuestionResponse: response content
-    """
-    logger = get_heavyiq_logger()
-    db = HeavyDB.from_session(request.session_id, include_tables=request.tables)
-    logger.info("Request Received")
-    logger.info("Table(s): %s", ", ".join(request.tables))
-    nl_sql_llm = get_llm_by_type(LLMType.NL_TO_SQL, temperature=0.0)
-    nl_sql_chain = get_nl_to_sql_chain_by_llm(llm=nl_sql_llm)(
-        llm=nl_sql_llm, database=db, tags=["rest-api", "question-endpoint"]  # type: ignore
-    )
-    llm = get_llm_by_type(LLMType.SQL_TO_ANSWER, temperature=0.0)
-    chain = NLtoAnswerChain(
-        llm=llm,
-        nl_sql_chain=nl_sql_chain,
-        database=db,
-        tags=["rest-api", "question-endpoint"],
-    )
-    chain_input = {chain.input_key: request.question, "tables": request.tables}
-    res = log_chain_call(chain, chain_input, "")
-    feedback_id = str(res["__run"].run_id) if "__run" in res else ""
-    return QuestionResponse(
-        answer=res[chain.output_key],
-        sql=res[chain.output_sql_key],
-        sql_complexity=res[chain.output_sql_complexity_key],
-        feedback_id=feedback_id,
     )
 
 
