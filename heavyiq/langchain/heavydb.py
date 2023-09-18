f075ea8e7b24ff2cb1d61a32439af63b8a580d70
Todd Mostak <todd@omnisci.com>
2023-09-18 09:00:00 -0700

    [IQ-97] Ensure that reserved keywords are quoted for get_column_top_k

--- heavyiq/langchain/heavydb.py
+++ heavyiq/langchain/heavydb.py
@@ -292,6 +292,24 @@ class HeavyDB:
         config = get_config()
         cardinality_threshold = config.column_top_k_cardinality_threshold
         high_cardinality_sample = config.column_top_k_high_cardinality_sample
+
+        # We need to ensure that column names that are reserved keywords are double quoted otherwise an error will occur
+        # Todo (todd): These are only a partial list of reserved keywords, ensure we have an exhaustive list
+        reserved_keywords = [
+            "LANGUAGE",
+            "RANK",
+            "RESULT",
+            "DATE",
+            "TIMESTAMP",
+            "LENGTH",
+            "YEAR",
+            "QUARTER",
+            "MONTH",
+            "WEEK",
+            "DAY",
+        ]
+        if column.upper() in reserved_keywords:
+            column = f'"{column}"'
         # check to see if the column is low cardinality
         # fetch top (threshold + 1)
         # if there are < (threshold + 1) values, it's low cardinality and we can return all of them
