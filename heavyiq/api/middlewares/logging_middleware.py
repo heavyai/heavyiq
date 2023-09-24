bea93ae072a943d8a2db49e1af38ad3883a99f96
Avinash <avistylein3105@gmail.com>
2023-09-24 20:08:55 +0530

    instead of middleware, support for logging request/response body from route class

--- heavyiq/api/middlewares/logging_middleware.py
+++ heavyiq/api/middlewares/logging_middleware.py
@@ -1,4 +1,3 @@
-import json
 from pydantic import BaseModel
 from typing import Literal, Any
 from fastapi.concurrency import run_in_threadpool
@@ -7,54 +6,7 @@ from starlette.responses import Response
 from starlette.background import BackgroundTask
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from heavyiq.logging_utils import _get_access_logger, get_heavyiq_logger, HeavyIQLogger, _AccessLogger
-from heavyiq.api.utils import AsyncIteratorWrapper
-
-
-class LoggingMiddleware(BaseHTTPMiddleware):
-    """
-    Supports logging before and after each http request.
-    """
-
-    async def _get_response_body(self, response: Response) -> tuple[Response, str]:
-        """
-        Gets the response body for logging from Response object.
-        """
-        resp_body_sections = [section async for section in response.__dict__["body_iterator"]]
-        response.__setattr__("body_iterator", AsyncIteratorWrapper(resp_body_sections))
-
-        try:
-            resp_body: str = json.loads(resp_body_sections[0].decode())  # type: ignore
-        except Exception:
-            resp_body = str(resp_body_sections)
-
-        return response, resp_body
-
-    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        """
-        Method responsible for dispatching request object to the appropriate handler.
-        """
-        is_api_request = "/api/v1/" in str(request.url)
-        # Code executed before the request is processed
-
-        if is_api_request:
-            get_heavyiq_logger().debug("Request Path: %s", request.url.path)
-
-        response = await call_next(request)
-
-        _get_access_logger().info("", extra={"response": response, "request": request})
-        # Code executed after the request has been processed
-        if not is_api_request:
-            return response
-
-        # log response body only for the api request
-        try:
-            reponse, response_body = await self._get_response_body(response)
-            get_heavyiq_logger().debug("Response Content: %s", response_body, extra={"request": request})
-        except RuntimeError:
-            # skip writing response data in-case of runtime error
-            pass
-        get_heavyiq_logger().debug("Response Status Code: %d", response.status_code, extra={"request": request})
-        return response
+from starlette.types import ASGIApp, Scope, Receive, Send, Message
 
 
 class Log(BaseModel):
@@ -70,7 +22,7 @@ class Log(BaseModel):
         arbitrary_types_allowed = True
 
 
-class AsyncLoggingMiddleware(LoggingMiddleware):
+class AsyncLoggingMiddleware(BaseHTTPMiddleware):
     """
     Async logging middleware.
     Most of the time, logging involves writing the log content to a file (blocking I/O operation).
@@ -114,22 +66,7 @@ class AsyncLoggingMiddleware(LoggingMiddleware):
         # create access log
         logs.append(Log(type=app_logger, level="info", message="", extra={"response": response, "request": request}))
 
-        # log response body only for the api request
         if is_api_request:
-            try:
-                reponse, response_body = await self._get_response_body(response)
-                logs.append(
-                    Log(
-                        type=heavyiq_logger,
-                        level="debug",
-                        message="Response Content: %s",
-                        values=[response_body],
-                        extra={"request": request},
-                    )
-                )
-            except RuntimeError:
-                # skip writing response data in-case of runtime error
-                pass
             logs.append(
                 Log(
                     type=heavyiq_logger,
@@ -142,3 +79,26 @@ class AsyncLoggingMiddleware(LoggingMiddleware):
 
         response.background = BackgroundTask(self.write_log_data, logs)
         return response
+
+
+class LogRequestsMiddleware:
+    def __init__(self, app: ASGIApp) -> None:
+        self.app = app
+        self.logger = get_heavyiq_logger()
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        async def send_with_logs(message: Message):
+            """Log every request info and response status code."""
+            if message["type"] == "http.response.start":
+                # request info is stored in the scope
+                # status code is stored in the message
+                await run_in_threadpool(
+                    self.logger.info,
+                    f'{scope["client"][0]}:{scope["client"][1]} - '
+                    f'"{scope["method"]} {scope["path"]} '
+                    f'{scope["scheme"]}/{scope["http_version"]}" '
+                    f'{message["status"]}',
+                )
+            await send(message)
+
+        await self.app(scope, receive, send_with_logs)
---
+++ heavyiq/api/routes/log_route.py
@@ -0,0 +1,41 @@
+from fastapi import Response, Request
+from starlette.background import BackgroundTask
+from starlette.responses import StreamingResponse
+from fastapi.routing import APIRoute
+from typing import Callable, Any
+from heavyiq.logging_utils import get_heavyiq_logger
+
+
+def log_info(req_body: Any, res_body: Any):
+    logging = get_heavyiq_logger()
+    logging.debug("Request Body: %s", req_body)
+    logging.debug("Response Body: %s", res_body)
+
+
+class LoggingRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            req_body: bytes = await request.body()
+            response = await original_route_handler(request)
+
+            if isinstance(response, StreamingResponse):
+                res_body = b""
+                async for item in response.body_iterator:
+                    res_body += item
+
+                task = BackgroundTask(log_info, req_body, res_body)
+                return Response(
+                    content=res_body,
+                    status_code=response.status_code,
+                    headers=dict(response.headers),
+                    media_type=response.media_type,
+                    background=task,
+                )
+            else:
+                res_body = response.body
+                response.background = BackgroundTask(log_info, req_body, res_body)
+                return response
+
+        return custom_route_handler
