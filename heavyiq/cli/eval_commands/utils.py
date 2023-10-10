2377068d5746d330241d97a38b3b9d375a6996b8
Todd Mostak <todd@heavy.ai>
2023-10-10 13:56:02 -0400

    Add logprob stats to eval output using new compute_prob_stats function

--- heavyiq/cli/eval_commands/utils.py
+++ heavyiq/cli/eval_commands/utils.py
@@ -9,9 +9,53 @@ import csv
 
 import pandas as pd
 import numpy as np
+import math
 
 from heavyiq.langchain import HeavyDB
 
+def compute_prob_stats(selected_tokens: list[str], top_log_probs: list[dict[str, float]]) -> dict[str, Any]:
+    selected_tokens_and_probs = []
+    sum_log_probs = 0.0
+    min_prob = 1.0
+    min_prob_token = None
+    prob_decile_histogram = [0] * 10
+    
+    num_calc_tokens = 0
+    select_seen = False
+    for selected_token, token_logprobs in zip(selected_tokens, top_log_probs):
+        selected_token_log_prob = token_logprobs[selected_token]
+        # Convert the log probability to actual probability
+        selected_token_prob = math.exp(selected_token_log_prob)
+        # We don't use the probabilities on any tokens up to and including the first SELECT, as depending on the 
+        # prompt the LLM will sometimes want to put a newline token first, so using these first probabilities
+        # will artifically lower the avg, total, and min probabilities
+
+        # Todo (Todd): Consider the case where we have a query starting with a WITH (CTE)
+
+        if select_seen:
+            num_calc_tokens += 1
+            sum_log_probs += selected_token_log_prob
+            prob_decile_histogram[int(selected_token_prob * 10)] += 1
+            if selected_token_prob < min_prob:
+                min_prob = selected_token_prob
+                min_prob_token = selected_token
+        else:
+            if selected_token == "▁SELECT" or selected_token == "SELECT":
+                select_seen = True
+            
+        # Append the result to the list
+        selected_tokens_and_probs.append({'token': selected_token, 'probability': selected_token_prob})
+
+    prob_stats = {}
+    prob_stats['num_tokens'] = len(selected_tokens)
+    prob_stats['total_prob'] = math.exp(sum_log_probs)
+    prob_stats['avg_prob'] = math.exp(sum_log_probs / num_calc_tokens)
+    prob_stats['min_prob'] = min_prob
+    prob_stats['min_prob_token'] = min_prob_token
+    prob_stats['prob_decile_histogram'] = prob_decile_histogram
+    prob_stats['selected_token_probs'] = selected_tokens_and_probs
+    
+    return prob_stats
 
 def sql_rate_reply(db_id: str, gold_query: str, pred_query: str) -> dict[str, Any]:
     # motivated by https://github.com/lm-sys/FastChat/tree/main/fastchat/pred
@@ -80,13 +124,14 @@ def sql_rate_reply(db_id: str, gold_query: str, pred_query: str) -> dict[str, An
         print(pred_query)
         return query_metadata
 
-
-async def awrite_eval_results_header(eval_str: str, has_id: bool):
+async def awrite_eval_results_header(eval_str: str, has_id: bool, enable_logprobs):
     async with aiofiles.open(f"./eval/results/{eval_str}_results.csv", "a", newline="") as wf:
+        header = []
         if has_id:
-            header = ["id", "db_id", "gold_query", "pred_query", "success", "status", "error"]
-        else:
-            header = ["db_id", "gold_query", "pred_query", "success", "status", "error"]
+            header.append("id")
+        header.extend(["db_id", "gold_query", "pred_query", "success", "status", "error"])
+        if enable_logprobs:
+            header.extend(["num_tokens", "total_prob", "avg_prob", "min_prob", "prob_decile_histogram"])
         writer = AsyncWriter(wf, dialect="unix")
         await writer.writerow(header)
 
@@ -100,15 +145,19 @@ async def awrite_eval_results_row(
     pred_query: str = "",
     error: Optional[str] = None,
     query_id: Optional[str] = None,
+    prob_stats: Optional[dict] = None
 ):
     async with aiofiles.open(f"./eval/results/{eval_str}_results.csv", "a", newline="") as wf:
         writer = AsyncWriter(wf, dialect="unix")
         # Clean the data
-        row_data = [db_id, gold_query, pred_query, success, status, error or ""]
-        row_data = [str(item).replace("\n", " ").replace("\r", " ") for item in row_data]
-
+        row_data = []
         if query_id:
-            row_data.insert(0, query_id)
+            row_data.append(query_id)
+        row_data.extend([db_id, gold_query, pred_query, success, status, error or ""])
+        if prob_stats:
+            row_data.extend([prob_stats["num_tokens"], prob_stats["total_prob"], prob_stats["avg_prob"], prob_stats["min_prob"], prob_stats["prob_decile_histogram"]])
+
+        row_data = [str(item).replace("\n", " ").replace("\r", " ") for item in row_data]
 
         await writer.writerow(row_data)
 
