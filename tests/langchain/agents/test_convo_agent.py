8f6446d1dd50e5eb7b3a62c3deb4c25d54276ffa
Avinash <avistylein3105@gmail.com>
2023-10-21 00:28:16 +0530

    altered convo agent tests w.r.t latest changes

--- tests/langchain/agents/test_convo_agent.py
+++ tests/langchain/agents/test_convo_agent.py
@@ -1,21 +1,23 @@
 import re
+import pytest
 from unittest.mock import patch
 from langchain.agents import AgentExecutor
 from heavyiq.langchain import HeavyDB
 from langchain.schema import HumanMessage, AIMessage
 from heavyiq.langchain.agents.convo_agent import create_conversational_agent
-from tests import override_config
+from tests import aoverride_config
 
 
-@override_config
-def test_should_create_and_run_chat_sql_agent_which_saves_last_run_sql_into_memory(
+@pytest.mark.asyncio
+@aoverride_config
+async def test_should_create_and_run_chat_sql_agent_which_saves_last_run_sql_into_memory(
     mock_heavy_db: HeavyDB, heavyiq_config
 ):
     agent: AgentExecutor = create_conversational_agent(mock_heavy_db)
     assert isinstance(agent, AgentExecutor)
     question = "What is the total population in the USA according to the data in the usa_states table?"
-    response = agent.run(question)
-    assert re.match(r"(?s).*?total population.*?\s+usa_states\s+.*?\d+.*", response)
+    response = await agent.arun(question)
+    assert re.match(r"(?is).*?total population.*?\s+usa.*?\d+.*", response)
     last_run_sql_messages = agent._heavyiq_buffer_memory.load_memory_variables({})["last_run_sql"]  # type: ignore
     assert last_run_sql_messages
     # previous message to the last message should be HumanMessage
@@ -23,4 +25,5 @@ def test_should_create_and_run_chat_sql_agent_which_saves_last_run_sql_into_memo
     assert isinstance(human_message, HumanMessage)
     assert human_message.content == question
     assert isinstance(ai_message, AIMessage)
-    assert ai_message.content == "SELECT SUM(population) FROM usa_states;"
+    # result column may or maynot get aliased
+    assert re.search(r"(?i)SELECT SUM\(population\)(?: as \w+)? FROM usa_states;", ai_message.content)
--- tests/langchain/chains/heavydb/test_nl_to_sql_chat_chain.py
+++ tests/langchain/chains/heavydb/test_nl_to_sql_chat_chain.py
@@ -1,22 +1,18 @@
 import pytest
 from heavyiq.langchain import HeavyDB
-from heavyiq.langchain.logging import log_chain_call_async, log_chain_call
+from tests import aoverride_config
+from heavyiq.langchain.logging import log_chain_call_async
 from heavyiq.langchain.chains import NLtoSQLChatChain
 from tests.langchain import FakeChatOpenAI
 
 
 @pytest.mark.asyncio
-async def test_nl_to_sql_chat_async_chain_should_pass(mock_heavy_db: HeavyDB, fake_chat_llm: FakeChatOpenAI):
+@aoverride_config
+async def test_nl_to_sql_chat_async_chain_should_pass(
+    mock_heavy_db: HeavyDB, fake_chat_llm: FakeChatOpenAI, heavyiq_config
+):
     chain = NLtoSQLChatChain(llm=fake_chat_llm, database=mock_heavy_db)
     chain_input = {chain.input_key: "", "tables": ["usa_states"]}
     res = await log_chain_call_async(chain, chain_input, "")
-    chain_output = res[chain.output_key]
-    assert chain_output == "SELECT SUM(POPULATION) AS total_population\nFROM usa_states;"
-
-
-def test_nl_to_sql_chat_sync_chain_should_pass(mock_heavy_db: HeavyDB, fake_chat_llm: FakeChatOpenAI):
-    chain = NLtoSQLChatChain(llm=fake_chat_llm, database=mock_heavy_db)
-    chain_input = {chain.input_key: "", "tables": ["usa_states"]}
-    res = log_chain_call(chain, chain_input, "")
     chain_output = res[chain.output_key]
     assert chain_output == "SELECT SUM(POPULATION) AS total_population\nFROM usa_states;"
