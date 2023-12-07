11b9e8272e4a120247fbb0a831e0c48348a2a100
Avinash <avistylein3105@gmail.com>
2023-12-07 21:06:07 +0530

    updated top_k_column_count default value

--- heavyiq/config/config_schema.py
+++ heavyiq/config/config_schema.py
@@ -56,7 +56,7 @@ class HeavyIQConfig(OverrideBaseConfig):  # type: ignore
     custom_llm_logprobs_limit: int = 1
     # top-k
     top_k_max_str_col_count_nl_to_sql: int = 30
-    top_k_max_str_col_count_nl_to_tables: int = 20
+    top_k_max_str_col_count_nl_to_tables: int = 30
 
 
 class AppConfig(OverrideBaseConfig):  # type: ignore
