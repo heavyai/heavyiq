a6970437e5a27e1becc9407631abdbe5ba4c9c49
Avinash <avistylein3105@gmail.com>
2023-12-08 17:55:02 +0530

    Fixed testcases

--- tests/api/test_lcel.py
+++ tests/api/test_lcel.py
@@ -99,7 +99,27 @@ def test_should_pass_lcel_auto_question_endpoint(heavyiq_config, client, session
 
 
 @override_config
-def test_should_pass_lcel_answer_endpoint(heavyiq_config, client, session_id):
+def test_should_pass_lcel_nl_to_answer_endpoint(heavyiq_config, client, session_id):
+    """
+    Test LCEL answer endpoint.
+    """
+
+    payload = {
+        "session_id": session_id,
+        "question": "How many states begin with the letter A? What are they?",
+        "tables": ["usa_states"],
+    }
+    response = client.post("/api/v1/lcel/nl-to-answer", json=payload)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json["answer"]
+    assert response_json["sql"]
+    assert response_json["sql_result"]
+    assert response_json["sql_complexity"] in [3, 4]
+
+
+@override_config
+def test_should_pass_lcel_sql_to_answer_endpoint(heavyiq_config, client, session_id):
     """
     Test LCEL answer endpoint.
     """
@@ -108,12 +128,11 @@ def test_should_pass_lcel_answer_endpoint(heavyiq_config, client, session_id):
         "session_id": session_id,
         "query": "SELECT COUNT(DISTINCT STATE_NAME) AS count_states, STATE_NAME FROM usa_states WHERE STATE_NAME LIKE 'A%' GROUP BY STATE_NAME;",
         "question": "How many states begin with the letter A? What are they?",
-        "tables": ["usa_states"],
     }
-    response = client.post("/api/v1/lcel/answer", json=payload)
+    response = client.post("/api/v1/lcel/sql-to-answer", json=payload)
     assert response.status_code == 200
     response_json = response.json()
     assert response_json["answer"]
     assert response_json["sql"]
     assert response_json["sql_result"]
-    assert response_json["sql_complexity"] == 3
+    assert response_json["sql_complexity"] in [3, 4]
