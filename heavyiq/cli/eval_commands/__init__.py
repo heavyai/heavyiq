372c6edd7077aaf96bd99ebfff42c8d1c63db47e
Avinash <avistylein3105@gmail.com>
2023-12-17 02:17:32 +0530

    Implemented new eval cmd run_config_model_on_auto_questions

--- heavyiq/cli/eval_commands/__init__.py
+++ heavyiq/cli/eval_commands/__init__.py
@@ -253,3 +253,90 @@ async def run_config_model_on_tables(
                 rows_written += len(row_datas)
 
     await run_in_threadpool(summarize_eval_results, eval_str)
+
+
+@eval.command()
+@coro
+@click.option("--temperature", default=0.0, help="Temperature for LLM (Defaults to 0.0)", type=float)
+@click.option("--verbose", default=False, help="Verbose output", type=bool)
+@click.option("--enable-query-stats", default=True, help="Enable Query Stats?", type=bool)
+@click.option("--batch-size", default=10, help="How many questions to process parallelly?", type=int)
+@click.argument("eval_dataset_csv", type=str)
+@click.pass_context  # type: ignore
+async def run_config_model_on_auto_questions(
+    ctx: click.Context,
+    eval_dataset_csv: str,
+    batch_size: int,
+    temperature: float,
+    verbose: bool,
+    enable_query_stats: bool,
+) -> None:
+    from heavyiq.langchain.utils import is_langsmith_active
+    from heavyiq.lcel.chains.eval.eval_auto_table_question_chain import chain
+
+    logger = get_heavyiq_logger()
+
+    if not await run_in_threadpool(os.path.exists, eval_dataset_csv):
+        raise Exception(f"eval_dataset_csv does not exist: {eval_dataset_csv}")
+
+    eval_id = uuid4().hex[:8]
+    eval_str = f"eval_{eval_id}"
+    logger.info(f"Dataset ID: {eval_id}")
+
+    rows_written: int = 0
+    eval_results_csv: str = f"./eval/results/{eval_str}_results.csv"
+
+    # csv must have columns: id (optional), db_id, tables, question, answer
+    async with aiofiles.open(eval_dataset_csv, mode="r") as f, aiofiles.open(eval_results_csv, "a", newline="") as wf:
+        csv_reader, writer = AsyncReader(f), AsyncWriter(wf, dialect="unix")
+        header = await anext(csv_reader)
+        has_id = "id" == header[0]  # type: ignore
+
+        # write header
+        header_data = ["db_id", "gold_query", "pred_query", "success", "status", "error"]
+        if has_id:
+            header_data = ["id"] + header_data
+        if enable_query_stats:
+            header_data.extend(["num_joins", "num_unions", "num_aggs", "num_filters", "num_sorts"])
+        await writer.writerow(header_data)
+
+        while True:
+            rows = []
+            for _ in range(batch_size):
+                try:
+                    rows.append(await csv_reader.__anext__())
+                except StopAsyncIteration:
+                    pass
+            if not rows:
+                break
+            print(f"Processing {len(rows)} rows...")
+            batch_inputs = []
+            for row in rows:
+                if has_id:
+                    query_id, db_id, question, answer = row
+                else:
+                    query_id = None
+                    db_id, question, answer = row
+                batch_inputs.append(
+                    {
+                        "query_id": query_id,
+                        "db_id": db_id,
+                        "question": question,
+                        "sql": answer,
+                        "enable_query_stats": enable_query_stats,
+                    }
+                )
+            if not batch_inputs:
+                break
+
+            chain_output = await chain.abatch(batch_inputs, config={"configurable": {"llm_temperature": temperature}})
+            row_datas = []
+            for data in chain_output:
+                values_list = list(data.values())
+                query_stats = list(values_list[-1].values())
+                row_datas.append(values_list[:-1] + query_stats)
+            if row_datas:
+                await writer.writerows(row_datas)
+                rows_written += len(row_datas)
+
+    await run_in_threadpool(summarize_eval_results, eval_str)
---
+++ heavyiq/lcel/chains/eval/eval_auto_table_question_chain.py
@@ -0,0 +1,111 @@
+import asyncio
+
+from fastapi.concurrency import run_in_threadpool
+from langchain.pydantic_v1 import BaseModel, Field
+from langchain.schema.runnable import Runnable, RunnableLambda, RunnablePassthrough
+
+from heavyiq.langchain.heavydb import HeavyDB
+
+from ..heavydb.auto_sql_chain import chain as auto_sql_chain
+
+
+class InputType(BaseModel):
+    """Chain input type."""
+
+    query_id: str | None = Field(default=None, description="Eval query id.")
+    db_id: str = Field(..., description="HeavyDB database name.")
+    question: str = Field(..., description="Natural Language question.")
+    sql: str | None = Field(default=None, description="HeavyDB compatible SQL query.")
+    enable_query_stats: bool = Field(default=True, description="Whether to calculate query stats or not.")
+
+
+class OutputType(BaseModel):
+    """Chain output type"""
+
+    query_id: str | None = Field(default=None, description="Eval query id.")
+    db_id: str = Field(..., description="HeavyDB database name.")
+    gold_query: str = Field(..., description="Gold Query.")
+    pred_query: str = Field(..., description="Predicted Query.")
+    success: bool = Field(..., description="Boolean flag which indicates whether the prediction went correct or not.")
+    status: str = Field(..., description="String which denotes the prediction status.")
+    error: str = Field(default="", description="stores the error string.")
+    query_stats: dict = Field(default={}, description="Query stats")
+
+
+async def get_session_id(inputs: dict) -> str:
+    """
+    Create a db session against the input db_id using the credenials from the config.
+    """
+    return await HeavyDB.create_session_id_async(db_name=inputs["db_id"])
+
+
+async def extract_gold_tables_from_query(inputs: dict) -> list[str]:
+    """
+    Helps to extract gold tables from the input SQL Query.
+    """
+    from heavyiq.cli.eval_commands.utils import extract_tables_from_query
+
+    heavydb = await HeavyDB.from_session_async(session_id=inputs["session_id"])
+    return extract_tables_from_query(heavydb._conn, inputs["sql"])
+
+
+async def compare_tables(inputs: dict) -> dict:
+    """
+    Compares the extracted and predicted tables.
+    """
+    gold_tables, pred_tables = sorted(inputs["gold_tables"]), sorted(inputs["pred_tables"])
+    error = ""
+    if gold_tables != pred_tables:
+        error = "TABLES_MISMATCH"
+    return {**inputs, "error": error, "tables": pred_tables}
+
+
+async def compare_and_format_output(inputs: dict) -> dict:
+    """
+    Formats the llm results according to the chain's output schema.
+    """
+    from heavyiq.cli.eval_commands.utils import sql_rate_reply
+
+    sql_outputs = inputs["sql_outputs"]
+    gold_tables, pred_tables, gold_query, pred_query, error = (
+        sorted(inputs["gold_tables"]),
+        sorted(sql_outputs["tables"]),
+        inputs["sql"],
+        sql_outputs["query"],
+        sql_outputs["error"],
+    )
+    if gold_tables != pred_tables:
+        error = "TABLES_MISMATCH"
+
+    db = HeavyDB.from_env(db_name=inputs["db_id"])
+    tasks = [run_in_threadpool(sql_rate_reply, gold_query, pred_query, db=db)]
+    if inputs["enable_query_stats"]:
+        tasks.append(db.aquery_stats(pred_query))
+    else:
+        async_lambda = asyncio.coroutine(lambda x: {})
+        tasks.append(async_lambda)
+    eval_res, query_stats = await asyncio.gather(*tasks)
+
+    return {
+        "query_id": inputs["query_id"],
+        "db_id": inputs["db_id"],
+        "gold_query": gold_query,
+        "pred_query": pred_query,
+        "success": eval_res["success"],
+        "status": eval_res["status"],
+        "error": error or eval_res["error"],
+        "query_stats": query_stats,
+    }
+
+
+get_session_id_lambda: Runnable = RunnableLambda(get_session_id)
+extract_gold_tables: Runnable = RunnableLambda(extract_gold_tables_from_query)
+
+chain: Runnable = (
+    RunnablePassthrough().assign(session_id=get_session_id_lambda)
+    | RunnablePassthrough().assign(gold_tables=extract_gold_tables)
+    | RunnablePassthrough.assign(sql_outputs=auto_sql_chain)
+    | RunnableLambda(compare_and_format_output)  # type: ignore
+).with_types(
+    input_type=InputType, output_type=OutputType  # type: ignore
+)
