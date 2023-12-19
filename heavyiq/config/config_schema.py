e3f82137837cc4a7af7747f02d638adc6b77b847
Todd Mostak <tmostak@gmail.com>
2023-12-19 13:21:32 -0500

    Do not include row samples in custom table document generation by default

--- heavyiq/config/config_schema.py
+++ heavyiq/config/config_schema.py
@@ -63,7 +63,7 @@ class HeavyIQConfig(OverrideBaseConfig):  # type: ignore
     # index updation and search will happen in-order to find relevant tables.
     allowed_tables_max_count_nl_to_tables: int = 20
     # config options w.r.t table doc generation using custom model
-    include_samples_in_custom_table_document_generation: bool = True
+    include_samples_in_custom_table_document_generation: bool = False
     include_top_k_in_custom_table_document_generation: bool = True
     # nl to sql max query retries
     max_retries_nl_to_sql: int = 3
