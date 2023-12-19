01214404b2d65da8c25869a0b3421e2e99952f4d
Avinash <avistylein3105@gmail.com>
2023-12-19 08:50:56 +0530

    typo

--- heavyiq/lcel/chains/eval/eval_auto_table_question_chain.py
+++ heavyiq/lcel/chains/eval/eval_auto_table_question_chain.py
@@ -75,20 +75,21 @@ async def compare_and_format_output(inputs: dict) -> dict:
         sql_outputs["query"],
         sql_outputs["error"],
     )
-    if gold_tables != pred_tables:
-        error = "TABLES_MISMATCH"
+
+    has_tables_mismatch = True if gold_tables != pred_tables else False
 
     # has error then pred query failed to pass the validation step
     # or the tables might get mismatched. In this case, don't calculate sql_rate_reply and query_stats
     # just return the function with necessary error details
-    if error:
+    if error or has_tables_mismatch:
+        status = "tables_mismatch" if has_tables_mismatch else "failed_to_generate_sql"
         return {
             "query_id": inputs["query_id"],
             "db_id": inputs["db_id"],
             "gold_query": gold_query,
             "pred_query": pred_query,  # failed sql
             "success": False,
-            "status": "tables_mismatch" if error == "TABLES_MISMATCH" else "failed_to_generate_sql",
+            "status": status,
             "error": error,
             "query_stats": {},
         }
