331673856f9b5348d4059385cdc307c5f540b7f7
Avinash <avistylein3105@gmail.com>
2023-12-22 17:09:18 +0530

    cal-llm endpoint now attached to the default router

--- heavyiq/api/routes/default_router.py
+++ heavyiq/api/routes/default_router.py
@@ -1,8 +1,11 @@
 import os
 
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import RedirectResponse, FileResponse
+from fastapi.concurrency import run_in_threadpool
+from fastapi.responses import FileResponse, RedirectResponse
 
+from heavyiq.api.models import CallLLMRequest, CallLLMResponse
+from heavyiq.langchain.llms import get_llm_by_type
 
 defaultrouter = APIRouter()
 
@@ -15,10 +18,22 @@ async def redirect_to_docs() -> RedirectResponse:
 
 
 @defaultrouter.get("/version.txt")
-async def serve_version():
+async def serve_version() -> FileResponse:
     file_path = os.path.join(PUBLIC_PATH, "version.txt")
 
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="Not Found")
 
     return FileResponse(file_path)
+
+
+@defaultrouter.post("/call-llm", response_model=CallLLMResponse)
+async def call_llm(request: CallLLMRequest) -> CallLLMResponse:
+    """
+    Call LLM.
+    """
+    llm = await run_in_threadpool(
+        get_llm_by_type, request.llm_type, temperature=request.temperature, max_tokens=request.max_tokens
+    )
+    response = await llm.apredict(request.prompt, stop=request.stop)
+    return CallLLMResponse(response=response.strip())
