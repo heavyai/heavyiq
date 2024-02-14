bb52139bde39d17251c607e322d6eb948989a42b
Avinash <avistylein3105@gmail.com>
2024-02-14 21:24:25 +0530

    typo

--- heavydb_reader/base.py
+++ heavydb_reader/base.py
@@ -2,8 +2,11 @@ import logging
 import re
 from typing import Any, List, Optional
 
-from heavydb.connection import connect
+from heavyai.connection import connect
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.node_parser.node_utils import IdFuncCallable, build_nodes_from_splits
 from llama_index.core.readers.base import BaseReader, Document
+from llama_index.core.schema import MetadataMode, TextNode
 
 logger = logging.getLogger(__name__)
 
@@ -28,11 +31,11 @@ def is_destructive_sql(sql: str) -> bool:
     return sql_parts[0] in destructive_statements
 
 
-def get_table_names_from_sql(sql: str) -> List[str]:
+def get_table_name_from_sql(sql: str) -> List[str]:
     """
     Get table names from the input query.
     """
-    return re.findall(r"(?i)(?:from|join)\s+(\S+)", sql)
+    return re.findall(r"(?i)from\s+(\S+)", sql)
 
 
 class HeavyDBReader(BaseReader):
@@ -87,6 +90,7 @@ class HeavyDBReader(BaseReader):
             protocol=protocol,
             sessionid=sessionid,
         )
+        self.dbname = dbname
 
     def execute_query(self, query_string: str) -> List[Any]:
         """
@@ -125,13 +129,39 @@ class HeavyDBReader(BaseReader):
 
         try:
             result = self.execute_query(query)
-            tables = get_table_names_from_sql(query)
+            table = get_table_name_from_sql(query)
 
             for item in result:
                 # create document for each item
                 doc_str = ", ".join([str(entry) for entry in item])
-                documents.append(Document(text=doc_str, metadata={"source": tables}))
+                documents.append(Document(text=doc_str, metadata={"table": table, "database": self.dbname}))
         except Exception as e:
             logger.error("An error occurred while loading the data: {}".format(e), exc_info=True)
 
         return documents
+
+    def load_table_schema(self, table_name: str) -> List[TextNode]:
+        """
+        Grab table schema from database, form a parent node for table comment and the relevant child nodes for the columns.
+        """
+        # Get table comment and each column comment from thrift endpoint
+        table_details = self.connection._client.get_table_details(self.connection._session, table_name)
+        table_comment = getattr(table_details.refresh_info, "comment", f"{table_name}: table comment")
+        table_document = Document(
+            text=table_comment,
+            metadata={"table": table_name, "database": self.dbname},
+            metadata_seperator="::",
+            metadata_template="{key}=>{value}",
+            text_template="Metadata: {metadata_str}\n-----\nContent:\n{content}",
+        )
+
+        splits = []
+        for col in table_details.row_desc:
+            column_name = col.col_name
+            column_comment = getattr(col, "comment", f"{table_name}.{column_name}: column comment")
+            splits.append(column_comment)
+
+        # create table_comment as separate Document and the relevant columns as
+        # TextNode's having the parent as table_comment
+        nodes = build_nodes_from_splits(splits, table_document)
+        return nodes
