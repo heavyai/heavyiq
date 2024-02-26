866abe9fea497ae5915d98a1ff930ace9edf6489
Avinash <avistylein3105@gmail.com>
2024-02-26 22:49:47 +0530

    updated  tcs w.r.t heavydb index

--- tests/test_metadata_index.py
+++ tests/test_metadata_index.py
@@ -1,5 +1,7 @@
 import unittest
-from typing import AsyncIterator
+from collections.abc import AsyncIterator
+from typing import Any
+from unittest.mock import patch
 
 import pytest
 
@@ -11,19 +13,26 @@ async def metadata_index() -> AsyncIterator[HeavyDBMetadataIndex]:
     """
     Async metadata index fixture.
     """
-    # this should generate missing documents and then
+    # this should generate missing documents (only when table_documents been emptied) and then
     # populates the index with those documents
-    result = await aget_heavydb_index()
-    yield result
+    # if the index already exists then the documents related to "usa_states", "flights_2008" tables
+    # gets updated
+    with patch(
+        "heavyiq.langchain.index.heavydb.create_index.agenerate_table_documents",
+        return_value=["usa_states", "flights_2008"],
+    ):
+        result = await aget_heavydb_index()
+        yield result
 
 
 @pytest.mark.anyio
-async def test_simple_search_for_table_names(heavyiq_config, metadata_index: HeavyDBMetadataIndex):
-    table_names = await metadata_index.asimple_search_for_table_names(
-        "How many states begin with the letter A?", allowable_tables=["usa_states"]
-    )
-    assert table_names
+async def test_simple_search_for_table_names(heavyiq_config: Any, metadata_index: HeavyDBMetadataIndex):
+    table_names = await metadata_index.asimple_search_for_table_names("How many states begin with the letter A?")
     assert "usa_states" in table_names
+    table_names = await metadata_index.asimple_search_for_table_names(
+        "What were the total number of flights that were cancelled in 2008?"
+    )
+    assert "flights_2008" in table_names
 
 
 if __name__ == "__main__":
