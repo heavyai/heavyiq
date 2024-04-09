a849c1496b10f0448c4ad41453004489470a2bd8
Avinash Raj <avistylein3105@gmail.com>
2024-04-10 03:33:01 +0530

    Make custom prompt templates configurable (#239)
    * Make custom prompt templates configurable
    
    * Added default values for custom prompt start and end tokens
    
    * Split default prompt into parts (start token, body, end token)

--- heavyiq/config/config_schema.py
+++ heavyiq/config/config_schema.py
@@ -101,6 +101,31 @@ class HeavyIQConfig(OverrideBaseConfig):  # type: ignore
     max_retries_nl_to_sql: int = 2
     # llm api-key
     heavylm_api_key: Optional[str] = None
+    # prompts
+    # nl to sql
+    custom_prompt_nl_to_sql_start_token: str = "<|sql prompt|>\n"
+    custom_prompt_nl_to_sql_body: Optional[str] = None
+    custom_prompt_nl_to_sql_end_token: str = "\n<|sql answer|>\n"
+    # nl to sql error
+    custom_prompt_nl_to_sql_error_start_token: str = "<|sql error prompt|>\n"
+    custom_prompt_nl_to_sql_error_body: Optional[str] = None
+    custom_prompt_nl_to_sql_error_end_token: str = "\n<|sql error answer|>\n"
+    # sql to answer
+    custom_prompt_sql_to_answer_start_token: str = "<|english prompt|>\n"
+    custom_prompt_sql_to_answer_body: Optional[str] = None
+    custom_prompt_sql_to_answer_end_token: str = "\n<|english answer|>\n"
+    # nl to tables
+    custom_prompt_nl_to_tables_start_token: str = "<|table prompt|>\n"
+    custom_prompt_nl_to_tables_body: Optional[str] = None
+    custom_prompt_nl_to_tables_end_token: str = "\n<|table answer|>\n"
+    # tables to questions
+    custom_prompt_tables_to_questions_start_token: str = "<|table question prompt|>\n"
+    custom_prompt_tables_to_questions_body: Optional[str] = None
+    custom_prompt_tables_to_questions_end_token: str = "\n<|table question answer|>\n"
+    # instruct
+    custom_prompt_instruct_start_token: str = "<|prompt|>\n"
+    custom_prompt_instruct_body: Optional[str] = None
+    custom_prompt_instruct_end_token: str = "\n<|prompt|>\n"
 
 
 class AppConfig(OverrideBaseConfig):  # type: ignore
---
+++ heavyiq/lcel/prompts/default.py
@@ -0,0 +1,78 @@
+DEFAULT_NL_TO_SQL_PROMPT = (
+    "<|sql prompt|>\n",
+    """You are an experienced data analyst adept at writing SQL queries to answer user questions.
+
+You have access to the following relational tables, with schemas below.
+
+{table_info}
+
+Write a SQL query to answer the following question:
+{input}""",
+    "\n<|sql answer|>\n",
+)
+
+
+DEFAULT_NL_TO_SQL_ERROR_PROMPT = (
+    "<|sql error prompt|>\n",
+    """You generated a SQL query that generated an exception when executed in the HeavyDB database.
+
+You have access to the following relation tables, with schemas below.
+
+{table_info}
+
+In attempting to answer the following user question:
+
+{input},
+you generated the following SQL query:
+{sql_cmd}
+, which failed to run in the HeavyDB database, generating the following error:
+{error}
+
+Please alter the query to run without error in HeavyDB:""",
+    "\n<|sql error answer|>\n",
+)
+
+DEFAULT_SQL_TO_ANSWER_PROMPT = (
+    "<|english prompt|>\n",
+    """The user asked the following question:
+{input}
+
+By considering the following columns:
+{columns}
+
+To answer the question, the following SQL query was generated:
+{sql_cmd}
+
+The following results were returned:
+{sql_result}
+
+Now explain the results in English, referencing the question and the SQL query as needed.""",
+    "\n<|english answer|>\n",
+)
+
+DEFAULT_NL_TO_TABLES_PROMPT = (
+    "<|table prompt|>\n",
+    """You are an experienced data analyst adept at analyzing user questions to determine the SQL tables required to generate an answer.
+You have access to the following relational tables, with schemas below.
+
+{table_info}
+
+Emit each table name along with a 1 if the table is required to answer the following user question, or a 0 if the table is not required.
+
+Question: {input}
+""",
+    "\n<|table answer|>\n",
+)
+
+DEFAULT_TABLES_TO_QUESTIONS_PROMPT = (
+    "<|table question prompt|>\n",
+    """You are an experienced data analyst adept at asking compelling questions of your data.
+You have access to the following relational tables, with schemas below.
+
+{table_info}
+
+Write a compelling question to ask of the above data:""",
+    """\n<|table question answer|>\n""",
+)
+
+DEFAULT_INSTRUCT_PROMPT = ("<|prompt|>\n", "{question}", "\n<|answer|>\n")
