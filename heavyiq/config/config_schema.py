e09712cd78963e521fa14649e5613646f53eb799
Todd Mostak <tmostak@gmail.com>
2024-05-03 18:23:55 -0400

    Update default context window from 8192 to to 16384, col counts from 30 to 50

--- heavyiq/config/config_schema.py
+++ heavyiq/config/config_schema.py
@@ -48,15 +48,15 @@ class HeavyIQConfig(OverrideBaseConfig):  # type: ignore
     custom_llm_type: Optional[str] = None
     """ 'API' or 'API_VLLM' or 'AZURE' """
     custom_llm_api_base: str = ""  # LLMType.DEFAULT
-    custom_llm_api_context_window: int = 8192  # LLMType.DEFAULT
+    custom_llm_api_context_window: int = 16384  # LLMType.DEFAULT
     custom_llm_api_nl_to_sql_base: Optional[str] = None  # LLMType.NL_TO_SQL
-    custom_llm_api_nl_to_sql_context_window: int = 8192  # LLMType.NL_TO_SQL
+    custom_llm_api_nl_to_sql_context_window: int = 16384  # LLMType.NL_TO_SQL
     custom_llm_api_sql_to_answer_base: Optional[str] = None  # LLMType.SQL_TO_ANSWER
-    custom_llm_api_sql_to_answer_context_window: int = 8192  # LLMType.SQL_TO_ANSWER
+    custom_llm_api_sql_to_answer_context_window: int = 16384  # LLMType.SQL_TO_ANSWER
     custom_llm_api_nl_to_tables_base: Optional[str] = None  # LLMType.NL_TO_TABLES
-    custom_llm_api_nl_to_tables_context_window: int = 8192  # LLMType.NL_TO_TABLES
+    custom_llm_api_nl_to_tables_context_window: int = 16384  # LLMType.NL_TO_TABLES
     custom_llm_api_instruct_base: Optional[str] = None  # LLMType.INSTRUCT
-    custom_llm_api_instruct_context_window: int = 8192  # LLMType.INSTRUCT
+    custom_llm_api_instruct_context_window: int = 16384  # LLMType.INSTRUCT
     custom_llm_api_instruct_prompt_start_token: str = "<|prompt|>\n"
     custom_llm_api_instruct_prompt_end_token: str = "\n<|answer|>\n"
     custom_llm_api_vllm_beam_width: int = 2
@@ -68,8 +68,8 @@ class HeavyIQConfig(OverrideBaseConfig):  # type: ignore
     enable_logprobs: bool = False
     custom_llm_logprobs_limit: int = 1
     # top-k
-    top_k_max_str_col_count_nl_to_sql: int = 30
-    top_k_max_str_col_count_nl_to_tables: int = 30
+    top_k_max_str_col_count_nl_to_sql: int = 50
+    top_k_max_str_col_count_nl_to_tables: int = 50
     # table order in prompt
     sort_prompt_tables_desc = True
     # maximum count of allowed_tables on nl-to-tables endpoint. If the count exceeds, then
