86a4653893f64ff6e1177c35c3b0790e286f1759
Joe O'Hallaron <joe.ohallaron@gmail.com>
2024-05-20 15:22:45 -0600

    [FE-17775] Update config values when using free edition, allow disabling IQ from config (#256)
    * Butchering config defaults based on license type
    
    * Cant track if config is provided...
    
    * sys.exit when disabled or non-free edition with no config, other stuff
    
    * Cleanup, don't need a blank data source
    
    * Fix typo in community url
    
    * Default llm type, remove commented out code
    
    * Remove resolved comment
    
    * Remove free edition llm type default as it is the overall default now

--- heavyiq/config/__init__.py
+++ heavyiq/config/__init__.py
@@ -1,7 +1,6 @@
 import os
 import threading
 from typing import Any
-from urllib.parse import urlparse
 
 from confz import FileSource
 from heavydb.thrift.Heavy import Client  # type: ignore
@@ -33,55 +32,66 @@ def get_heavydb_license_claims(config: HeavyIQConfig) -> TLicenseInfo:
         )
 
 
-def validate_config(app_config: AppConfig) -> AppConfig:
+def validate_config(iq: HeavyIQConfig) -> AppConfig:
     """
     Validates app config after it's creation.
     This involves basic validation of configs and creation of directory mentioned if not exists.
     """
-    if app_config.iq.custom_llm_type is None or app_config.iq.custom_llm_type == "AZURE":
-        if app_config.iq.custom_llm_type == "AZURE" and (
-            app_config.iq.custom_llm_azure_deployment_name.strip() == ""
-            or app_config.iq.custom_llm_azure_openai_api_base.strip() == ""
-            or app_config.iq.custom_llm_azure_openai_api_version.strip() == ""
+    if iq.custom_llm_type is None or iq.custom_llm_type == "AZURE":
+        if iq.custom_llm_type == "AZURE" and (
+            iq.custom_llm_azure_deployment_name.strip() == ""
+            or iq.custom_llm_azure_openai_api_base.strip() == ""
+            or iq.custom_llm_azure_openai_api_version.strip() == ""
         ):
             raise ValueError(
                 "Custom LLM type is set to 'AZURE', but deployment name or API base URL or API Version is not set."
             )
-        openai_client = OpenAI(api_key=app_config.iq.openai_api_key)
-        if app_config.iq.custom_llm_type is None:
+        openai_client = OpenAI(api_key=iq.openai_api_key)
+        if iq.custom_llm_type is None:
             try:
                 openai_client.models.list()
             except Exception as e:
                 raise ValueError(f"Unable to communicate with OpenAI API: {e}")
-    elif app_config.iq.custom_llm_type == "API":
-        if app_config.iq.custom_llm_api_base.strip() == "":
+    elif iq.custom_llm_type == "API":
+        if iq.custom_llm_api_base.strip() == "":
             raise ValueError("Custom LLM type is set to 'API', but API base URL is not set.")
-    elif app_config.iq.custom_llm_type == "API_VLLM":
-        if app_config.iq.custom_llm_api_base.strip() == "":
+    elif iq.custom_llm_type == "API_VLLM":
+        if iq.custom_llm_api_base.strip() == "":
             raise ValueError("Custom LLM type is set to 'API_VLLM', but API base URL is not set.")
     else:
         raise ValueError(
-            f"Invalid custom LLM type (valid options are AZURE or API or API_VLLM): {app_config.iq.custom_llm_type}"
+            f"Invalid custom LLM type (valid options are AZURE or API or API_VLLM): {iq.custom_llm_type}"
         )
-    if app_config.iq.data is None:
-        if app_config.data:
-            app_config.iq.data = app_config.data
-        else:
-            app_config.iq.data = "./storage"
-    if not os.path.exists(app_config.iq.data):
-        os.makedirs(app_config.iq.data)
+    
 
-    return app_config
+    return iq
 
 
-def get_config(file: str = "./config.toml") -> HeavyIQConfig:
+def get_config(file: str = "./config.toml", config_provided = True) -> HeavyIQConfig:
     """If called with a non-default file path, must be called before importing any other modules that use the config."""
     global _config
+
     with _config_lock:
         if _config:
             return _config
-        app_config: AppConfig = AppConfig(config_sources=FileSource(file=file))  # type: ignore
-        app_config = validate_config(app_config)
+        
+        app_config: AppConfig
+        if config_provided:
+            app_config = AppConfig(config_sources=FileSource(file=file))  # type: ignore
+            validate_config(app_config.iq)
+        else:
+            app_config = AppConfig()
+            print("Skipping validation until license is loaded and config updated per edition used.")
+
+        # Data setup things, move out of validate method
+        if app_config.iq.data is None:
+            if app_config.data:
+                app_config.iq.data = app_config.data
+            else:
+                app_config.iq.data = "./storage"
+        if not os.path.exists(app_config.iq.data):
+            os.makedirs(app_config.iq.data)
+
         _config = app_config.iq
 
         return _config
@@ -108,11 +118,18 @@ def change_iq_config_for_free_edition() -> bool:
     """
 
     keys_to_change = {
-        "custom_llm_api_base": "https://api2.heavy.ai/v1",
-        "custom_llm_api_nl_to_sql_base": "https://api2.heavy.ai/v1",
-        "custom_llm_api_sql_to_answer_base": "https://api2.heavy.ai/v1",
-        "custom_llm_api_nl_to_tables_base": "https://api2.heavy.ai/v1",
-        "custom_llm_api_tables_to_questions_base": "https://api2.heavy.ai/v1",
-        "custom_llm_api_instruct_base": "https://api2.heavy.ai/v1",
+        "heavydb_port": 6274,
+        "custom_llm_api_base": "https://community.heavylm.heavy.ai/v1", # base url of API",
+        "custom_llm_api_nl_to_sql_base": "https://community.heavylm.heavy.ai/v1",
+        "custom_llm_api_sql_to_answer_base": "https://community.heavylm.heavy.ai/v1",
+        "custom_llm_api_nl_to_tables_base": "https://community.heavylm.heavy.ai/v1",
+        "custom_llm_api_tables_to_questions_base": "https://community.heavylm.heavy.ai/v1",
+        "custom_llm_api_instruct_base": "https://community.heavylm.heavy.ai/v1",
     }
-    return change_iq_config(keys_to_change)
+
+    change_success = change_iq_config(keys_to_change)
+
+    global _config
+    validate_config(_config)
+    
+    return change_success
