2c52f37ad3431434d0d43469fc1f5ddb60beb133
Todd Mostak <tmostak@gmail.com>
2024-06-07 09:25:34 -0400

    Drop column_top_k_max_unique_values_count default to 5M (#269)

--- heavyiq/config/config_schema.py
+++ heavyiq/config/config_schema.py
@@ -35,7 +35,7 @@ class HeavyIQConfig(OverrideBaseConfig):  # type: ignore
     """If the cardinality of a column is less than this threshold, we will include each value of the column in the prompt"""
     column_top_k_high_cardinality_sample: int = 3
     """If the cardinality of a column is greater than the threshold, we will sample this many values from the column"""
-    column_top_k_max_unique_values_count: int = 10000000  # 10 Million
+    column_top_k_max_unique_values_count: int = 5000000  # 5 Million
     """Maximum number of unqiue values a column might posses in-order to find top-k"""
     # TELEMETRICS
     langchain_endpoint: str = "https://api.smith.langchain.com"
