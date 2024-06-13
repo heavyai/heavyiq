18eaa50a48253ee994db6136688555222ed7fe72
Todd Mostak <tmostak@gmail.com>
2024-06-12 23:44:35 -0400

    [IQ-162] Add optional guided choice tokens to call-llm to constrain output (#275)

--- heavyiq/api/routes/llm_router.py
+++ heavyiq/api/routes/llm_router.py
@@ -19,5 +19,12 @@ async def call_llm(request: CallLLMRequest) -> CallLLMResponse:
         get_llm_by_type, LLMType.INSTRUCT, temperature=request.temperature, max_tokens=request.max_tokens
     )
     prompt = f"{config.custom_llm_api_instruct_prompt_start_token}{request.question}{config.custom_llm_api_instruct_prompt_end_token}"
-    response = await llm.apredict(prompt, stop=request.stop, callbacks=[NoopLangChainTracer(client=1)])
+    response = await llm.apredict(
+        prompt,
+        stop=request.stop,
+        extra_body={
+            "guided_choice": request.guided_choice
+        }, # Passed as part of kwargs
+        callbacks=[NoopLangChainTracer(client=1)]
+    )
     return CallLLMResponse(response=response.strip())
