714869c1eecf29c0e568972091532788f813d0d9
Todd Mostak <tmostak@gmail.com>
2024-07-10 18:56:34 -0400

    Change max length of output for call-llm from 512 to 768
    (cherry picked from commit 0d31cd2ac8aac6e1c4f1f4458f63e92e96984230)

--- heavyiq/api/models/llm.py
+++ heavyiq/api/models/llm.py
@@ -6,7 +6,7 @@ from heavyiq.langchain.llms import LLMType
 class CallLLMRequest(BaseModel):
     question: str = Field(..., description="Actual NL question.")
     temperature: float = Field(0.0, description="Temperature to use for LLM")
-    max_tokens: int = Field(512, description="Max tokens to use for LLM response")
+    max_tokens: int = Field(768, description="Max tokens to use for LLM response")
     guided_choice: list[str] | None = Field(default=None, description="An optional list of allowed outputs")
     guided_regex: str | None = Field(default=None, description="An optional regex pattern to guide output")
     stop: list[str] | None = Field(default=None, description="An optional list of stop words")
@@ -17,7 +17,7 @@ class CallLLMRequest(BaseModel):
                 {
                     "question": "Return whether the following is a city, state, or country: Idaho",
                     "temperature": 0.0,
-                    "max_tokens": 512,
+                    "max_tokens": 768,
                     "guided_choice": ["city", "state", "country"],
                     "stop": ["is the capital"],
                 }
