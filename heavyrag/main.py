49a74157435eeea9650aa8a8368a34c540f92ee9
Avinash Raj <avistylein3105@gmail.com>
2024-10-18 22:20:15 +0530

    added support for fetching guidance without embedding server (#326)
    * added support for fetching guidane without embedding server
    
    * re-arranged import
    
    * Fixed issue with swappable vector database and empty/null embedding server.

--- heavyrag/main.py
+++ heavyrag/main.py
@@ -6,25 +6,40 @@ from llama_index.core.base.response.schema import RESPONSE_TYPE
 from llama_index.core.evaluation import EvaluationResult
 from llama_index.core.postprocessor import SimilarityPostprocessor
 from llama_index.core.response_synthesizers import ResponseMode
-from llama_index.core.schema import NodeWithScore
+from llama_index.core.schema import NodeWithScore, TextNode
 
 from heavyiq.langchain.heavydb import HeavyDB
 from heavyrag import IndexNotFound
+from heavyrag.database import FactsModel, ragdb
+from heavyrag.embed import get_embed_model
 from heavyrag.evaluate import aevaluate_response_by_relevancy
 from heavyrag.filters import document_filters, get_document_filter_matches, get_facts_filter_matches, table_filters
-from heavyrag.ingest import sync_table_index
 from heavyrag.llm import get_llm
 from heavyrag.logger import logger
-from heavyrag.postprocessor import OverridedLLMRerank
+from heavyrag.postprocessor import OverridedLLMRerank, ReRanker, ReRankerType
 from heavyrag.prompts import FACTS_QA_PROMPT, TEXT_QA_PROMPT
 from heavyrag.utils import get_document_node_count_in_index, get_facts_node_count_in_index, get_nodes
 
 
-def get_index(collection_name: str) -> VectorStoreIndex:
+def list_database_facts(database_name: str) -> list[tuple[str, str]]:
+    """
+    List all the facts/snippets releavnt to a database.
+    """
+    with ragdb.get_db() as session:
+        facts = FactsModel.list(db_session=session, heavydb_name=database_name)
+        snippets = [(i.id, i.fact) for i in facts]  # type: ignore
+        return snippets  # type: ignore
+
+
+def get_index(collection_name: str) -> None | VectorStoreIndex:
     """
     Get Vecstor Store Index.
     """
     from heavyrag.controller import rag_controller
+
+    if not get_embed_model():
+        # no embed server, so we can't embed the question for doing RAG search against the vectordb
+        return None
 
     return rag_controller.index(collection_name)
 
@@ -57,6 +72,26 @@ async def ask_document(
     return response, eval_result
 
 
+async def filter_snippets_using_reranker(
+    question: str, snippets: list[tuple[str, str]], reranker_top_k: int = 3, reranker_cutoff: float = 0.01
+) -> list[NodeWithScore]:
+    """
+    Filter relevant snippets from all available snippets using a reranker.
+    This is useful when the RAG embedding server is disabled and you need to filter out relevant snippets
+    by passing all available snippets to the reranker prompt.
+    """
+    logger.debug("Started filtering snippets using re-ranker alone...")
+    reranker = ReRanker(
+        reranker_type=ReRankerType.OverridedLLMReRanker, top_n=reranker_top_k, cutoff_score=reranker_cutoff
+    )
+    text_nodes = [NodeWithScore(node=TextNode(id_=i, text=j)) for i, j in snippets]
+    nodes = await reranker._apostprocess_nodes(nodes=text_nodes, query_str=question)
+    logger.debug(
+        f"Node count after applying reranker with top_n and cutoff as {reranker_top_k}, {reranker_cutoff}: {len(nodes)}"
+    )
+    return nodes
+
+
 async def retrieve_facts(
     index: VectorStoreIndex,
     question: str,
@@ -70,10 +105,6 @@ async def retrieve_facts(
     """
     Retrieve only the fact nodes which are relevant to the asked question without using llm.
     """
-    from heavyiq.config import get_config
-
-    config = get_config()
-
     logger.debug("Started retrieving facts...")
 
     doc_nodes_with_score, facts_nodes_with_score = [], []
@@ -267,6 +298,10 @@ async def determine_table_names(question: str, heavydb: HeavyDB, force_sync: boo
     """
     Fetch the approprate table name relevant to the asked question.
     """
+    if not get_embed_model():
+        # no embed server, so we can't embed the question for doing RAG search against the vectordb
+        return []
+
     logger.debug("Started determining table names, syncing table index...")
     from heavyrag.controller import rag_controller
 
