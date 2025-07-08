86b101eff664600d25a894e1217ddea5f48d1c35
Avinash <avistylein3105@gmail.com>
2025-07-08 12:19:51 +0530

    reduced the chunk size to avoid exact chunk splitting w.r.t additon of chunk metadata

--- heavyrag/transform.py
+++ heavyrag/transform.py
@@ -4,14 +4,18 @@ from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core.schema import BaseNode, Document
 from llama_index.core.text_splitter import TokenTextSplitter
 
+from heavyiq.langchain.utils import custom_model_tokenizer_encode
 from heavyrag.embed import EMBED_MODEL_MAX_LEN
 
 # EMBED_MODEL_MAX_LEN variable being set on master process and then made available to all worker processes via --preload option
 # this option copies all the global varibales to the forked process
 if EMBED_MODEL_MAX_LEN:
-    token_splitter = TokenTextSplitter(chunk_size=EMBED_MODEL_MAX_LEN, chunk_overlap=20)
+    chunk_size = EMBED_MODEL_MAX_LEN - 50  # 50 as a buffer token
+    # Because LlamaIndex adds metadata (extra_info, metadata) when building the final chunk string,
+    # and this contributes to the total token count.
+    token_splitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=20, tokenizer=custom_model_tokenizer_encode)
 else:
-    token_splitter = TokenTextSplitter(chunk_size=512, chunk_overlap=20)
+    token_splitter = TokenTextSplitter(chunk_size=512, chunk_overlap=20, tokenizer=custom_model_tokenizer_encode)
 
 DEFAULT_PIPELINE = IngestionPipeline(
     transformations=[token_splitter]  # no need for SentenceSplitter if token chunking suffices
