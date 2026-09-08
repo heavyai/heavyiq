3297c8bedd34f2526f2bf82830617c4ebe319073
Avinash Raj <avistylein3105@gmail.com>
2026-09-08 21:54:46 +0530

    Remove unused Dockerfiles, environment helpers, and experiments (#384)
    * removed dev dockerfiles
    
    * Remove unused environment and experiments folders
    
    * bring back dockerfile.dev
    
    * Add license statement to docker-compose.yml

--- environment/start_embed_server.sh
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-model=BAAI/bge-large-en-v1.5
-revision=refs/pr/5
-volume=$PWD/embeddings_data # share a volume with the Docker container to avoid downloading weights every run
-
-docker run --gpus all -p 8080:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:1.2 --model-id $model --revision $revision
